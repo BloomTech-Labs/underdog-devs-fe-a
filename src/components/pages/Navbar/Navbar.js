bd8794ff01146fb14586a1b5454b8254825a6b41
Broveku <gearhearjordan@gmail.com>
2021-11-08 13:55:10 -0800

    Refactoring of Navbar for simple clean feature implementaion

--- src/components/pages/Navbar/Navbar.js
+++ src/components/pages/Navbar/Navbar.js
@@ -1,21 +1,26 @@
-import { Link } from 'react-router-dom';
 import React from 'react';
+import { Button } from 'antd';
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <h1>Underdog Devs!</h1>
       <nav>
-        <Link to="/component1">component1</Link>
-        <Link to="/component2">component2</Link>
-        <Link to="/component3">component3</Link>
-        <Link to="/component4">component4</Link>
-        <Link to="/component5">component5</Link>
-        <Link to="/component6">component6</Link>
-        <Link to="/component7">component7</Link>
-        <Link to="/component8">component8</Link>
-        <Link to="/component9">component9</Link>
-        <Link to="/component10">component10</Link>
+        <Button type="link" href="/examplefeature">
+          Feature1
+        </Button>
+        <Button type="link" href="/examplefeature">
+          Feature2
+        </Button>
+        <Button type="link" href="/examplefeature">
+          Feature3
+        </Button>
+        <Button type="link" href="/examplefeature">
+          Feature4
+        </Button>
+        <Button type="link" href="/examplefeature">
+          Feature5
+        </Button>
       </nav>
     </div>
   );
