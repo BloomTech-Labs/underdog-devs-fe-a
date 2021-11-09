700b970b12f0774164332b4ce31ee149bdb501ac
Thomas Bielawski <tfbielawski@protonmail.com>
2021-11-09 18:27:34 -0500

    Styled Navbar according to wireframe design. Created and styled a footer component that will be persistent throughout the app.

---
+++ src/components/pages/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => (
+  <div className="footer">
+    <div className="leftFooter">
+      <p>LEFT CONTENT</p>
+    </div>
+    <div className="middleFooter">
+      <p>MIDDLE CONTENT</p>
+    </div>
+    <div className="rightFooter">
+      <p>RIGHT CONTENT</p>
+    </div>
+  </div>
+);
+
+export default Footer;
