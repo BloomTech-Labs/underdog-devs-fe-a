956fe3a8f88521aaa9446569efd5b5b7a11ba19c
Thomas Bielawski <tfbielawski@protonmail.com>
2021-11-09 20:06:12 -0500

    Styled the footer with social media assets and content placeholders.

--- src/components/pages/Footer/Footer.js
+++ src/components/pages/Footer/Footer.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import './Footer.css';
+import github from '../Footer/github5.svg';
+import youtube from '../Footer/youtube2.svg';
+import twitter from '../Footer/twitter10.svg';
+import gmail from '../Footer/gmail2.svg';
 
 const Footer = () => (
   <div className="footer">
     <div className="leftFooter">
-      <p>LEFT CONTENT</p>
+      <div className="svg">
+        <img src={github} />
+        <img src={twitter} />
+        <img src={youtube} />
+        <img src={gmail} />
+      </div>
     </div>
     <div className="middleFooter">
-      <p>MIDDLE CONTENT</p>
+      <div>
+        <p className="middleText">Copyright 2021</p>
+      </div>
     </div>
     <div className="rightFooter">
-      <p>RIGHT CONTENT</p>
+      <div>
+        <p className="rightText">About</p>
+        <p className="rightText">Our Mission</p>
+        <p className="rightText">Contact</p>
+        <p className="rightText">Donate</p>
+      </div>
     </div>
   </div>
 );
