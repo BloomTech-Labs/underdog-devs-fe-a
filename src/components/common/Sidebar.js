2ede7f321f3e8adebe0c3c9d06b3de353186cfa8
Retrofitt <rafaelmendozajr94@gmail.com>
2021-12-08 15:35:18 -0800

    finished first conditional render for unique sidebar buttons

--- src/components/common/Sidebar.js
+++ src/components/common/Sidebar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import 'antd/dist/antd.css';
 import './styles/Sidebar.css';
 import { Layout, Menu, Switch as Toggle } from 'antd';
@@ -17,13 +17,13 @@ import RenderUpdateProfile from '../pages/UpdateProfile/RenderUpdateProfile';
 const { Content, Sider } = Layout;
 const { SubMenu } = Menu;
 
-const Sidebar = (props) => {
-  const {userInfo, authService} = props;
+const Sidebar = props => {
+  const { userInfo, authService } = props;
 
   const [collapsed, setCollapsed] = useState(false);
   const [render, updateRender] = useState(1);
 
-  const onCollapse = (collapsed) => {
+  const onCollapse = collapsed => {
     setCollapsed(collapsed);
   };
 
@@ -33,12 +33,12 @@ const Sidebar = (props) => {
   };
 
   const components = {
-    1: <CalendarFeature/>,
+    1: <CalendarFeature />,
     2: <div>Option 2</div>,
     3: <div>Option 3</div>,
     4: <div>Option 4</div>,
     5: <div>Option 5</div>,
-    6: <RenderUpdateProfile/>,
+    6: <RenderUpdateProfile />,
     7: <div>Option 7</div>,
     8: <div>Option 8</div>,
     9: <div>Option 9</div>,
@@ -48,49 +48,72 @@ const Sidebar = (props) => {
     updateRender(menu.key);
   };
 
+  const role = localStorage.role_id;
+
+  const isUserMentee = () => {
+    if (role === '4') {
+      return true;
+    }
+  };
+
   return (
     <Layout style={{ minHeight: '100vh' }}>
-        <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
-          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-            <SubMenu key="sub1" icon={<CalendarOutlined />} title="Schedule">
-              <Menu.Item key="1" onClick={handleMenuClick}>
-                Calendar
+      <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+          <SubMenu key="sub1" icon={<CalendarOutlined />} title="Schedule">
+            <Menu.Item key="1" onClick={handleMenuClick}>
+              Calendar
+            </Menu.Item>
+            <Menu.Item key="2" onClick={handleMenuClick}>
+              Check Availabilities
+            </Menu.Item>
+            {isUserMentee() === true && (
+              <Menu.Item key="3" onClick={handleMenuClick}>
+                Schedule Meeting
               </Menu.Item>
-              <Menu.Item key="2" onClick={handleMenuClick}>Check Availabilities</Menu.Item>
-              <Menu.Item key="3" onClick={handleMenuClick}>Schedule Meeting</Menu.Item>
-            </SubMenu>
-            <SubMenu key="sub2" icon={<ContainerOutlined />} title="Tickets">
-              <Menu.Item key="4" onClick={handleMenuClick}>Open Ticket</Menu.Item>
-              <Menu.Item key="5" onClick={handleMenuClick}>Ticket Status</Menu.Item>
-            </SubMenu>
-            <SubMenu key="sub3" icon={<UserOutlined />} title="Account">
-              <Menu.Item key="6" onClick={handleMenuClick}>Profile Settings</Menu.Item>
-              <Menu.Item key="7" onClick={handleMenuClick}>Account Settings</Menu.Item>
-              <Menu.Item key="8" onClick={handleLogout}>Log Out</Menu.Item>
-            </SubMenu>
-            <Menu.Item key="9" icon={<DollarOutlined />}>
-              Donate
+            )}
+          </SubMenu>
+          <SubMenu key="sub2" icon={<ContainerOutlined />} title="Requests">
+            <Menu.Item key="4" onClick={handleMenuClick}>
+              Open Ticket
             </Menu.Item>
-            <Menu.Item key="10" icon={<QuestionCircleOutlined />}>
-              Support
+            <Menu.Item key="5" onClick={handleMenuClick}>
+              Ticket Status
             </Menu.Item>
-            <Menu.Divider />
-            <Menu.Item key="11" icon={<BulbOutlined />}>
-              <div id="darkmode">
-                Darkmode
-                <Toggle size="small" />
-              </div>
+          </SubMenu>
+          <SubMenu key="sub3" icon={<UserOutlined />} title="Account">
+            <Menu.Item key="6" onClick={handleMenuClick}>
+              Profile Settings
             </Menu.Item>
-          </Menu>
-        </Sider>
-        <Layout className="site-layout">
-          <Content style={{ margin: '2vh 1vw' }}>
-            <Content>{components[render]}</Content>
-          </Content>
-        </Layout>
+            <Menu.Item key="7" onClick={handleMenuClick}>
+              Account Settings
+            </Menu.Item>
+            <Menu.Item key="8" onClick={handleLogout}>
+              Log Out
+            </Menu.Item>
+          </SubMenu>
+          <Menu.Item key="9" icon={<DollarOutlined />}>
+            Donate
+          </Menu.Item>
+          <Menu.Item key="10" icon={<QuestionCircleOutlined />}>
+            Support
+          </Menu.Item>
+          <Menu.Divider />
+          <Menu.Item key="11" icon={<BulbOutlined />}>
+            <div id="darkmode">
+              Darkmode
+              <Toggle size="small" />
+            </div>
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Content style={{ margin: '2vh 1vw' }}>
+          <Content>{components[render]}</Content>
+        </Content>
       </Layout>
+    </Layout>
   );
 };
-
 
 export default Sidebar;
