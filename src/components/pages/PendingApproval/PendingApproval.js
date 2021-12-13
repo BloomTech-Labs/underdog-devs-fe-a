ecd7039ba6bfe3213596493cfa8c3fed324f51a6
Rick <ricksmycodeguy@gmail.com>
2021-12-13 16:39:32 -0600

    removes unnecessary console.log

--- src/components/pages/PendingApproval/PendingApproval.js
+++ src/components/pages/PendingApproval/PendingApproval.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react';
 import 'antd/dist/antd.css';
 import '../../common/styles/Sidebar.css';
 import { Layout, Menu, Switch as Toggle } from 'antd';
-import {
-  BulbOutlined,
-} from '@ant-design/icons';
+import { BulbOutlined } from '@ant-design/icons';
 
-import { DarkModeToggle, setTheme, getTheme } from '../../common/DarkModeToggle';
+import {
+  DarkModeToggle,
+  setTheme,
+  getTheme,
+} from '../../common/DarkModeToggle';
 
 const { Content, Sider } = Layout;
 
@@ -37,15 +39,13 @@ const PendingApproval = props => {
     localStorage.removeItem('role_id');
   };
 
-  console.log(userInfo);
-
   return (
     <Layout style={{ minHeight: '100vh' }}>
       <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
         <Menu theme="dark" mode="inline">
-            <Menu.Item key="10" onClick={handleLogout}>
-              Log Out
-            </Menu.Item>
+          <Menu.Item key="10" onClick={handleLogout}>
+            Log Out
+          </Menu.Item>
           <Menu.Divider />
           <Menu.Item key="13" icon={<BulbOutlined />}>
             <div id="darkmode">
@@ -63,7 +63,10 @@ const PendingApproval = props => {
       <Layout className="site-layout">
         <Content style={{ margin: '2vh 1vw' }}>
           <Content>
-            <div>Hey {userInfo.name}, currently your application is still pending. Please check back again soon!</div>
+            <div>
+              Hey {userInfo.name}, currently your application is still pending.
+              Please check back again soon!
+            </div>
           </Content>
         </Content>
       </Layout>
