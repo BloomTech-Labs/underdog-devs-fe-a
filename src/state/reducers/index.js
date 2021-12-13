c98b50e0050fb5940d5c3fba0b12751e2c0d8928
Rick <ricksmycodeguy@gmail.com>
2021-12-13 10:02:39 -0600

    Removes unnecessary files given nicks darmMode

--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -1,10 +1,7 @@
 // import all of your reducers into this file, and export them back out.
 // This allows for the simplification of flow when importing reducers into your actions throughout your app.
 import { combineReducers } from 'redux';
-import { darkModeReducer } from './darkModeReducer';
 
-const rootReducer = combineReducers({
-  darkModeReducer,
-});
+const rootReducer = combineReducers({});
 
 export default rootReducer;
