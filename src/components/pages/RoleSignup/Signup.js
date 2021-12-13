eb55a59f5d766fa4c7471eec850a5f699fbc2dd2
Teresa Franxman <teresafranxman97@gmail.com>
2021-12-13 12:11:00 -0600

    Created card components for mentee/mentor application options

--- src/components/pages/RoleSignup/Signup.js
+++ src/components/pages/RoleSignup/Signup.js
@@ -1,12 +1,61 @@
 import React from 'react';
-
-import Mentee from './Applications/Mentee';
-import Mentor from './Applications/Mentor';
+import { Link } from 'react-router-dom';
+import { Card, Button, Divider } from 'antd';
 
 const Signup = () => {
+  const { Meta } = Card;
+
   return (
-    <Mentor />
-    // <Mentee/>
+    <div className="container">
+      <div>
+        <h1>Signup</h1>
+      </div>
+      <Card
+        hoverable
+        style={{ width: 240 }}
+        cover={
+          <img
+            alt="example"
+            src="https://images.pexels.com/photos/7504837/pexels-photo-7504837.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+          />
+        }
+      >
+        <Meta
+          description="Ready to gain the technical skills required to become a Software Engineer?
+          Sign up to become a Mentee and connect with an industry expert!
+          "
+        />
+        <Link to="/menteeapplication">
+          <Button type="primary" block>
+            Mentee
+          </Button>
+        </Link>
+      </Card>
+
+      <Divider type="vertical" style={{ margin: '3em', height: '100px' }} />
+
+      <Card
+        hoverable
+        style={{ width: 240 }}
+        cover={
+          <img
+            alt="example"
+            src="https://images.pexels.com/photos/3861943/pexels-photo-3861943.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+          />
+        }
+      >
+        <Meta
+          description="Interested in supporting aspiring Software Engineers on their journey to achieve their goals? 
+            Sign up to become a Mentor and start teaching!
+            "
+        />
+        <Link to="/mentorapplication">
+          <Button type="primary" block>
+            Mentor
+          </Button>
+        </Link>
+      </Card>
+    </div>
   );
 };
 
