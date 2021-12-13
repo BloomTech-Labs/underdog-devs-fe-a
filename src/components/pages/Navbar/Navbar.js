ebde45987dd444c57f18152d617b5d5819bbfa38
Retrofitt <rafaelmendozajr94@gmail.com>
2021-12-13 14:24:25 -0800

    added profile avatar

--- src/components/pages/Navbar/Navbar.js
+++ src/components/pages/Navbar/Navbar.js
@@ -1,21 +1,37 @@
-import { Avatar, Button, Input } from 'antd';
-import React from 'react';
+import { Avatar, Image } from 'antd';
+import React, { useState } from 'react';
 import './Navbar.css';
 import logo from '../Navbar/ud_logo2.png';
-
+import { UserOutlined } from '@ant-design/icons';
 import { Layout, Menu, Breadcrumb } from 'antd';
 
 const { Header, Content, Footer } = Layout;
 
 const Navbar = () => {
+  const [profilePic, setProfilePic] = useState(
+    'https://joeschmoe.io/api/v1/random'
+  );
   return (
     <Layout className="layout">
       <Header>
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-          <div>
-            <img src={logo} alt="underdog devs logo" height="30" />
+          <div className="logo">
+            <img
+              src={logo}
+              alt="underdog devs logo"
+              height="68"
+              style={{ marginLeft: '1vw' }}
+            />
           </div>
-          <div>{/* profile stuff goes here */}</div>
+          <div style={{ display: 'flex' }}>
+            <div className="userInfo">
+              <div>{/* username stuff here */}</div>
+              <div>{/* role stuff here */}</div>
+              <div>
+                <Avatar icon={<UserOutlined />} src={profilePic} />
+              </div>
+            </div>
+          </div>
         </div>
       </Header>
     </Layout>
