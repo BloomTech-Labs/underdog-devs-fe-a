1370b3a0ea043c295b5e243c14a1548e5fbfd835
Retrofitt <rafaelmendozajr94@gmail.com>
2021-12-13 13:56:51 -0800

    replaces old header with antd header

--- src/components/pages/Navbar/Navbar.js
+++ src/components/pages/Navbar/Navbar.js
@@ -3,31 +3,22 @@ import React from 'react';
 import './Navbar.css';
 import logo from '../Navbar/ud_logo2.png';
 
+import { Layout, Menu, Breadcrumb } from 'antd';
+
+const { Header, Content, Footer } = Layout;
+
 const Navbar = () => {
   return (
-    <div className="navBar">
-      <div className="navLogo">
-        <img src={logo} alt="underdog devs logo" />
-        <p className="navLogoText">Underdog Devs</p>
-      </div>
-      <div className="navBoxRight">
-        <nav>
-          <Button type="link" className="navBarFont" href="/examplefeature">
-            Open Tickets
-          </Button>
-        </nav>
-        <Avatar.Group>
-          <Avatar
-            className="avatar"
-            shape="circle"
-            size="large"
-            style={{ backgroundColor: 'gray' }}
-          >
-            Avatar
-          </Avatar>
-        </Avatar.Group>
-      </div>
-    </div>
+    <Layout className="layout">
+      <Header>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <div>
+            <img src={logo} alt="underdog devs logo" height="30" />
+          </div>
+          <div>{/* profile stuff goes here */}</div>
+        </div>
+      </Header>
+    </Layout>
   );
 };
 
