d6a358775cb6fb27091dd65df68b2d976245054a
Teresa Franxman <teresafranxman97@gmail.com>
2021-12-14 17:41:48 -0600

    Created functional signup page with mentee/mentor application routing

--- src/components/pages/RoleSignup/Signup.js
+++ src/components/pages/RoleSignup/Signup.js
@@ -1,61 +1,87 @@
 import React from 'react';
+import './Styles/signup.css';
 import { Link } from 'react-router-dom';
-import { Card, Button, Divider } from 'antd';
+import { Card, Button, Row, Col, Divider, Space } from 'antd';
 
 const Signup = () => {
   const { Meta } = Card;
 
   return (
-    <div className="container">
-      <div>
-        <h1>Signup</h1>
-      </div>
-      <Card
-        hoverable
-        style={{ width: 240 }}
-        cover={
-          <img
-            alt="example"
-            src="https://images.pexels.com/photos/7504837/pexels-photo-7504837.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
-          />
-        }
-      >
-        <Meta
-          description="Ready to gain the technical skills required to become a Software Engineer?
-          Sign up to become a Mentee and connect with an industry expert!
-          "
-        />
-        <Link to="/menteeapplication">
-          <Button type="primary" block>
-            Mentee
-          </Button>
-        </Link>
-      </Card>
+    <Row className="row">
+      <Col sm={3} md={2} lg={1} className="col1">
+        <Card
+          className="card1"
+          hoverable
+          cover={
+            <img
+              alt="example"
+              src="https://images.pexels.com/photos/7504837/pexels-photo-7504837.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+              style={{ height: '100%', width: 300 }}
+            />
+          }
+        >
+          <Space
+            className="outer-space-container"
+            direction="horizontal"
+            align="center"
+          >
+            <Space direction="vertical">
+              <Meta className="title" description="Join our MENTEE program" />
+              <Meta description="Interested in acquiring the skills to become a Software Engineer? Apply to our Mentee program today to start learning." />
+            </Space>
 
-      <Divider type="vertical" style={{ margin: '3em', height: '100px' }} />
+            <Divider
+              className="divider"
+              type="vertical"
+              style={{ height: 100 }}
+            />
 
-      <Card
-        hoverable
-        style={{ width: 240 }}
-        cover={
-          <img
-            alt="example"
-            src="https://images.pexels.com/photos/3861943/pexels-photo-3861943.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
-          />
-        }
-      >
-        <Meta
-          description="Interested in supporting aspiring Software Engineers on their journey to achieve their goals? 
-            Sign up to become a Mentor and start teaching!
-            "
-        />
-        <Link to="/mentorapplication">
-          <Button type="primary" block>
-            Mentor
-          </Button>
-        </Link>
-      </Card>
-    </div>
+            <Link to="/menteeapplication">
+              <Button className="button" type="primary" block>
+                MENTEE
+              </Button>
+            </Link>
+          </Space>
+        </Card>
+      </Col>
+
+      <Col sm={3} md={2} lg={1} className="col2">
+        <Card
+          hoverable
+          className="card2"
+          cover={
+            <img
+              alt="example"
+              src="https://images.pexels.com/photos/1181337/pexels-photo-1181337.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+              style={{ height: '100%', width: 300 }}
+            />
+          }
+        >
+          <Space
+            className="outer-space-container"
+            direction="horizontal"
+            align="center"
+          >
+            <Space direction="vertical">
+              <Meta className="title" description="Join our MENTOR program" />
+              <Meta description="Do you have a passion for teaching and Software Engineering? Apply to become a Mentor and help support our Mentees." />
+            </Space>
+
+            <Divider
+              className="divider"
+              type="vertical"
+              style={{ height: 100 }}
+            />
+
+            <Link to="/mentorapplication">
+              <Button className="button" type="primary" block>
+                MENTOR
+              </Button>
+            </Link>
+          </Space>
+        </Card>
+      </Col>
+    </Row>
   );
 };
 
