4c673baf75c7ba35078818b838a59fb057e31af6
Rick <ricksmycodeguy@gmail.com>
2021-12-14 12:28:22 -0600

    adds actions suite for user

--- src/state/actions/index.js
+++ src/state/actions/index.js
@@ -1,5 +1,21 @@
-// import all of your actions into this file, and export them back out. 
+// import all of your actions into this file, and export them back out.
 // This allows for the simplification of flow when importing actions into your components throughout your app.
-// Actions should be focused to a single purpose. 
-// You can have multiple action creators per file if it makes sense to the purpose those action creators are serving. 
+// Actions should be focused to a single purpose.
+// You can have multiple action creators per file if it makes sense to the purpose those action creators are serving.
 // Declare action TYPES at the top of the file
+
+// USER ACTIONS
+export const getUserName = () => {
+  return dispatch => {
+    const token = localStorage.getItemBy('okta-token-storage');
+    dispatch(fetchUserName(JSON.parse(token).idToken.claims.name));
+  };
+};
+
+export const USER_NAME = 'USER_NAME';
+export const fetchUserName = user => {
+  return { type: USER_NAME, payload: user };
+};
+
+// CALENDAR ACTIONS
+// ADMIN TICKETS
