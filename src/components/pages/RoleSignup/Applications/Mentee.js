e0d66ff4d133a4c927ece886c04a2cd012804557
Retrofitt <rafaelmendozajr94@gmail.com>
2021-12-17 10:00:13 -0800

    Base Application Styles Implemented

--- src/components/pages/RoleSignup/Applications/Mentee.js
+++ src/components/pages/RoleSignup/Applications/Mentee.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Form, Input, Button, Radio } from 'antd';
+import './Styles/application.css';
 
 const initialFormValues = {
   email: '',
@@ -43,170 +44,176 @@ const Mentee = () => {
   };
 
   return (
-    <Form onFinish={formSubmit}>
-      <div>
-        <h1> Mentee Application </h1>
-        <div className="email">
-          <Form.Item
-            label="Email*"
-            type="email"
-            name="email"
-            value={formValues.email}
-            onChange={evt => {
-              inputChange('email', evt.target.value);
-            }}
-          >
-            <Input placeholder="Enter valid email" />
-          </Form.Item>
-        </div>
+    <div className="application">
+      <Form onFinish={formSubmit}>
+        <div className="signUpForm">
+          <h1> Mentee Application </h1>
+          <div className="questions">
+            <div className="infoDiv">
+              <div className="name">
+                <h3>Full Name*</h3>
+                <Form.Item
+                  type="text"
+                  name="name"
+                  value={formValues.name}
+                  onChange={evt => {
+                    inputChange('name', evt.target.value);
+                  }}
+                >
+                  <Input placeholder="Your Name" />
+                </Form.Item>
+              </div>
+              <div className="email">
+                <h3>Email*</h3>
+                <Form.Item
+                  type="email"
+                  name="email"
+                  value={formValues.email}
+                  onChange={evt => {
+                    inputChange('email', evt.target.value);
+                  }}
+                >
+                  <Input placeholder="Enter valid email" />
+                </Form.Item>
+              </div>
 
-        <div className="location">
-          <Form.Item
-            label="Location*"
-            type="text"
-            name="location"
-            value={formValues.location}
-            onChange={evt => {
-              inputChange('location', evt.target.value);
-            }}
-          >
-            <Input placeholder="Your Location" />
-          </Form.Item>
-        </div>
+              <div className="location">
+                <h3>Location*</h3>
+                <Form.Item
+                  type="text"
+                  name="location"
+                  value={formValues.location}
+                  onChange={evt => {
+                    inputChange('location', evt.target.value);
+                  }}
+                >
+                  <Input placeholder="Your Location" />
+                </Form.Item>
+              </div>
+            </div>
+            <hr />
+            <div className="lives_in_us">
+              <h3>Do you live in the U.S?*</h3>
+              <Radio.Group value={formValues.lives_in_us}>
+                <Radio
+                  onChange={evt => {
+                    inputChange('lives_in_us', evt.target.value);
+                  }}
+                  value="Yes"
+                >
+                  Yes
+                </Radio>
+                <Radio
+                  onChange={evt => {
+                    inputChange('lives_in_us', evt.target.value);
+                  }}
+                  value="No"
+                >
+                  No
+                </Radio>
+              </Radio.Group>
+            </div>
 
-        <div className="name">
-          <Form.Item
-            label="Name*"
-            type="text"
-            name="name"
-            value={formValues.name}
-            onChange={evt => {
-              inputChange('name', evt.target.value);
-            }}
-          >
-            <Input placeholder="Your Name" />
-          </Form.Item>
-        </div>
+            <div className="formerly_incarcerated">
+              <h3>Are you formerly incarcerated/have a felony?*</h3>
+              <Radio.Group value={formValues.formerly_incarcerated}>
+                <Radio
+                  onChange={evt => {
+                    inputChange('formerly_incarcerated', evt.target.value);
+                  }}
+                  value="Yes"
+                >
+                  Yes
+                </Radio>
+                <Radio
+                  onChange={evt => {
+                    inputChange('formerly_incarcerated', evt.target.value);
+                  }}
+                  value="No"
+                >
+                  No
+                </Radio>
+              </Radio.Group>
+            </div>
+            <hr />
+            <br />
+            <div className="list_convictions">
+              <h3>Please list your convictions if comfortable</h3>
+              <Form.Item
+                type="text"
+                name="list_convictions"
+                value={formValues.list_convictions}
+                onChange={evt => {
+                  inputChange('list_convictions', evt.target.value);
+                }}
+              >
+                <Input.TextArea placeholder="Your answer" />
+              </Form.Item>
+            </div>
 
-        <div className="lives_in_us">
-          <h3>Do you live in the U.S?*</h3>
-          <Radio.Group value={formValues.lives_in_us}>
-            <Radio
-              onChange={evt => {
-                inputChange('lives_in_us', evt.target.value);
-              }}
-              value="Yes"
-            >
-              Yes
-            </Radio>
-            <Radio
-              onChange={evt => {
-                inputChange('live_in_us', evt.target.value);
-              }}
-              value="No"
-            >
-              No
-            </Radio>
-          </Radio.Group>
-        </div>
+            <div className="tech_stack">
+              <h3>What is your tech stack?*</h3>
+              <Form.Item
+                type="text"
+                name="tech_stack"
+                value={formValues.tech_stack}
+                onChange={evt => {
+                  inputChange('tech_stack', evt.target.value);
+                }}
+              >
+                <Input.TextArea placeholder="Your answer" />
+              </Form.Item>
+            </div>
 
-        <div className="formerly_incarcerated">
-          <h3>Are you formerly incarcerated/have a felony?*</h3>
-          <Radio.Group value={formValues.formerly_incarcerated}>
-            <Radio
-              onChange={evt => {
-                inputChange('formerly_incarcerated', evt.target.value);
-              }}
-              value="Yes"
-            >
-              Yes
-            </Radio>
-            <Radio
-              onChange={evt => {
-                inputChange('formerly_incarcerated', evt.target.value);
-              }}
-              value="No"
-            >
-              No
-            </Radio>
-          </Radio.Group>
-        </div>
+            <div className="experience_level">
+              <h3>What is your level of experience?*</h3>
+              <Form.Item
+                type="text"
+                name="experience_level"
+                value={formValues.experience_level}
+                onChange={evt => {
+                  inputChange('experience_level', evt.target.value);
+                }}
+              >
+                <Input.TextArea placeholder="Your answer" />
+              </Form.Item>
+            </div>
 
-        <div className="list_convictions">
-          <h3>Please list your convictions if comfortable</h3>
-          <Form.Item
-            type="text"
-            name="list_convictions"
-            value={formValues.list_convictions}
-            onChange={evt => {
-              inputChange('list_convictions', evt.target.value);
-            }}
-          >
-            <Input placeholder="Your answer" />
-          </Form.Item>
-        </div>
+            <div className="your_hopes">
+              <h3>What are you hoping to gain from the community?*</h3>
+              <Form.Item
+                type="text"
+                name="your_hopes"
+                value={formValues.your_hopes}
+                onChange={evt => {
+                  inputChange('your_hopes', evt.target.value);
+                }}
+              >
+                <Input.TextArea placeholder="Your goals" />
+              </Form.Item>
+            </div>
 
-        <div className="tech_stack">
-          <h3>What is your tech stack?*</h3>
-          <Form.Item
-            type="text"
-            name="tech_stack"
-            value={formValues.tech_stack}
-            onChange={evt => {
-              inputChange('tech_stack', evt.target.value);
-            }}
-          >
-            <Input placeholder="Your answer" />
-          </Form.Item>
+            <div className="other_info">
+              <h3>Anything else you want us to know?</h3>
+              <Form.Item
+                type="text"
+                name="other_info"
+                value={formValues.other_info}
+                onChange={evt => {
+                  inputChange('other_info', evt.target.value);
+                }}
+              >
+                <Input.TextArea placeholder="Your answer" />
+              </Form.Item>
+            </div>
+            <hr />
+          </div>
+          <Button htmlType="submit" id="button">
+            Submit
+          </Button>
         </div>
-
-        <div className="experience_level">
-          <h3>What is your level of experience?*</h3>
-          <Form.Item
-            type="text"
-            name="experience_level"
-            value={formValues.experience_level}
-            onChange={evt => {
-              inputChange('experience_level', evt.target.value);
-            }}
-          >
-            <Input placeholder="Your answer" />
-          </Form.Item>
-        </div>
-
-        <div className="your_hopes">
-          <h3>What are you hoping to gain from the community?*</h3>
-          <Form.Item
-            type="text"
-            name="your_hopes"
-            value={formValues.your_hopes}
-            onChange={evt => {
-              inputChange('your_hopes', evt.target.value);
-            }}
-          >
-            <Input placeholder="Your goals" />
-          </Form.Item>
-        </div>
-
-        <div className="other_info">
-          <h3>Anything else you want us to know?</h3>
-          <Form.Item
-            type="text"
-            name="other_info"
-            value={formValues.other_info}
-            onChange={evt => {
-              inputChange('other_info', evt.target.value);
-            }}
-          >
-            <Input placeholder="Your answer" />
-          </Form.Item>
-        </div>
-
-        <Button htmlType="submit" id="button">
-          Submit
-        </Button>
-      </div>
-    </Form>
+      </Form>
+    </div>
   );
 };
 
