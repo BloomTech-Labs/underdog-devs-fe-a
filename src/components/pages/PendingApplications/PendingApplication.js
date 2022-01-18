9ce332d6376d40a27f49bc06ef0d1f0be3e15dcb
kennedybryant <kennedy_marisa96@yahoo.com>
2022-01-18 11:10:28 -0500

    Pending Application File Created

--- src/components/pages/PendingApplications/PendingApplication.js
+++ src/components/pages/PendingApplications/PendingApplication.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const PendingApplications = () => {
-  return <div></div>;
+  return (
+    <>
+      <h1>Pending Applications</h1>
+
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Role</th>
+          <th>Date</th>
+        </tr>
+      </table>
+    </>
+  );
 };
 
 export default PendingApplications;
