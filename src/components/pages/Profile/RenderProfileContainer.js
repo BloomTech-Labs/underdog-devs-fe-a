4c05993b3f36b4e615613020065ab03f6eeb1833
Daniel Kish <aox.soljah@gmail.com>
2022-01-21 04:00:07 +0700

    added all cards, no functionality completed at this time

--- src/components/pages/Profile/RenderProfileContainer.js
+++ src/components/pages/Profile/RenderProfileContainer.js
@@ -3,7 +3,14 @@ import { Link } from 'react-router-dom';
 import 'antd/dist/antd.css';
 // import '../Dashboard/Admin/index.css';
 import { Layout, Menu, Breadcrumb, Row, Col, Typography, Card } from 'antd';
+import { EditOutlined } from '@ant-design/icons';
+import {
+  TwitterOutlined,
+  LinkedinOutlined,
+  FacebookOutlined,
+} from '@ant-design/icons';
 import { Calendar } from 'antd';
+import { Skeleton } from 'antd';
 
 import {
   AppstoreOutlined,
@@ -84,7 +91,6 @@ const RenderProfileContainer = props => {
     <div>
       <Row style={{ height: '100vh', border: '1px solid red' }}>
         <Col span={6} style={{ border: '1px solid blue', padding: '3%' }}>
-          <Title level={2}>Testing 123</Title>
           <Card
             style={{ padding: '2%' }}
             cover={
@@ -94,16 +100,43 @@ const RenderProfileContainer = props => {
               />
             }
           >
-            <Meta description="123 testing" />
+            <Meta
+              title="address and contac info will go here soon i promise
+            testing
+            123"
+              description={<EditOutlined />}
+            />
+          </Card>
+          <Card
+            style={{ width: 300, marginTop: 16 }}
+            actions={[
+              <TwitterOutlined />,
+              <LinkedinOutlined />,
+              <FacebookOutlined />,
+            ]}
+          >
+            <Meta title="Socials" description={<EditOutlined />} />
           </Card>
         </Col>
         <Col span={18} style={{ border: '1px solid white' }}>
           <Title level={1}>Joe Schmoe</Title>
           <Card title="My Profile">
+            <Card type="inner" title="Bio" extra={<EditOutlined />}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+              vitae odio eu ante scelerisque pulvinar in vel arcu. Aenean a
+              varius massa. Nulla ac vehicula magna. Nam facilisis tellus ac
+              ipsum viverra maximus nec id quam. Donec mauris ligula, ultricies
+              ac sapien eu, placerat varius magna. Donec sit amet turpis quis
+              nisi blandit malesuada. Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Sed volutpat leo sed mi consequat, vel pharetra
+              ligula ultricies. Nam eu metus sodales, consectetur metus at,
+              aliquet sem.
+            </Card>
             <Card
+              style={{ marginTop: 16 }}
               type="inner"
-              title="Mentee Profile>"
-              extra={<a href="#">Edit</a>}
+              title="My Tech Stack"
+              extra={<EditOutlined />}
             >
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
               vitae odio eu ante scelerisque pulvinar in vel arcu. Aenean a
@@ -118,24 +151,8 @@ const RenderProfileContainer = props => {
             <Card
               style={{ marginTop: 16 }}
               type="inner"
-              title="My mentee Relationships"
-              extra={<a href="#">Edit</a>}
-            >
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-              vitae odio eu ante scelerisque pulvinar in vel arcu. Aenean a
-              varius massa. Nulla ac vehicula magna. Nam facilisis tellus ac
-              ipsum viverra maximus nec id quam. Donec mauris ligula, ultricies
-              ac sapien eu, placerat varius magna. Donec sit amet turpis quis
-              nisi blandit malesuada. Lorem ipsum dolor sit amet, consectetur
-              adipiscing elit. Sed volutpat leo sed mi consequat, vel pharetra
-              ligula ultricies. Nam eu metus sodales, consectetur metus at,
-              aliquet sem.
-            </Card>
-            <Card
-              style={{ marginTop: 16 }}
-              type="inner"
-              title="Mentoring Topics"
-              extra={<a href="#">Edit</a>}
+              title="My Mentoring Topics"
+              extra={<EditOutlined />}
             >
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
               vitae odio eu ante scelerisque pulvinar in vel arcu. Aenean a
