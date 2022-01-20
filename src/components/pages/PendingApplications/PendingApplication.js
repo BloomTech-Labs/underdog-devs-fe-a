9aef843c97d123d6ee4d99d8984fcb3dc667329f
kennedybryant <kennedy_marisa96@yahoo.com>
2022-01-20 13:48:30 -0500

    Styled Table Columns

--- src/components/pages/PendingApplications/PendingApplication.js
+++ src/components/pages/PendingApplications/PendingApplication.js
@@ -1,7 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import axiosWithAuth from '../../../utils/axiosWithAuth';
 
+import { Table } from 'antd';
 import './PendingApplication.css';
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    defaultSortOrder: 'descend',
+    sorter: (a, b) => a.name - b.name,
+  },
+  {
+    title: 'Role',
+    dataIndex: 'role',
+    filters: [
+      {
+        text: 'mentor',
+        value: 'mentor',
+      },
+      {
+        text: 'mentee',
+        value: 'mentee',
+      },
+    ],
+  },
+  {
+    title: 'Date',
+    dataIndex: 'date',
+    defaultSortOrder: 'descend',
+    sorter: (a, b) => a.date - b.date,
+  },
+];
 
 const PendingApplications = () => {
   const [applications, setApplications] = useState([]);
@@ -24,28 +54,28 @@ const PendingApplications = () => {
     <>
       <h2>Pending Applications</h2>
 
-      <table>
-        <tbody>
+      <Table columns={columns} dataSource={applications}>
+        {/* <tbody>
           <tr>
             <th>Name</th>
             <th>Role</th>
             <th>Date</th>
-          </tr>
+          </tr> */}
 
-          {applications.map((user, index) => {
-            let date = new Date(user.created_at);
-            return (
-              <tr key={index}>
-                <td>
-                  {user.first_name} {user.last_name}
-                </td>
-                <td>{user.role_name}</td>
-                <td>{date.toString()}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+        {applications.map((user, index) => {
+          let date = new Date(user.created_at);
+          return (
+            <tr key={index}>
+              <td>
+                {user.first_name} {user.last_name}
+              </td>
+              <td>{user.role_name}</td>
+              <td>{date.toString()}</td>
+            </tr>
+          );
+        })}
+        {/* </tbody> */}
+      </Table>
     </>
   );
 };
