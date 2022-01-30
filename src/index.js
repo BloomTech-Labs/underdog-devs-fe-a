236b3119c6c8d887bccb3c547007fcfc3cd0c2c6
riqthedev <riqthedev@gmail.com>
2022-01-29 19:51:08 -0500

    removed import on main index.js file

--- src/index.js
+++ src/index.js
@@ -32,7 +32,6 @@ import { Provider } from 'react-redux';
 import rootReducer from './state/reducers';
 import promiseMiddleware from 'redux-promise';
 import thunk from 'redux-thunk';
-import EditProfile from './components/pages/Profile/EditProfile';
 
 const store = createStore(
   rootReducer,
