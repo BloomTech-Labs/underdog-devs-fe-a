97ef5e945dcd7796b55c6eac38c1b90073432882
Brian Bellinger <brianpatrickbellinger@gmail.com>
2022-02-17 10:40:20 -0800

    matching component started

--- src/components/pages/MentorMenteeMatching/MentorMenteeMatching.js
+++ src/components/pages/MentorMenteeMatching/MentorMenteeMatching.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axiosWithAuth from '../../../utils/axiosWithAuth';
 
 const MentorMenteeMatching = () => {
+<<<<<<< HEAD
   const [assignments, setAssignments] = useState([]);
 
   useEffect(() => {
@@ -11,18 +12,52 @@ const MentorMenteeMatching = () => {
         .then(res => {
           console.log(res);
           //   setAssignments(res);
+=======
+  const [mentors, setMentors] = useState([]);
+  const [mentees, setMentees] = useState([]);
+
+  useEffect(() => {
+    const getMentors = () => {
+      axiosWithAuth()
+        //put correct endpoint
+        .get('/')
+        .then(res => {
+          setMentors(res);
+>>>>>>> bc81dae (matching component started)
         })
         .catch(err => {
           console.log(err);
         });
     };
+<<<<<<< HEAD
     getAssignments();
+=======
+    getMentors();
+  }, []);
+
+  useEffect(() => {
+    const getMentees = () => {
+      axiosWithAuth()
+        //put correct endpoint
+        .get('/')
+        .then(res => {
+          setMentees(res);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    };
+    getMentees();
+>>>>>>> bc81dae (matching component started)
   }, []);
 
   return (
     <>
       <h2>Matching</h2>
+<<<<<<< HEAD
       <p>list here</p>
+=======
+>>>>>>> bc81dae (matching component started)
     </>
   );
 };
