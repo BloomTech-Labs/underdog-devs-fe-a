ad242cc90dc42d341961251bb12ca8f0acf02fd5
[Tristan Harrower] <[harrowertristan@gmail.com]>
2022-02-22 19:31:11 -0500

    created mentee application css

--- src/components/pages/RoleSignup/Applications/Mentee.js
+++ src/components/pages/RoleSignup/Applications/Mentee.js
@@ -15,7 +15,7 @@ import {
   ReconciliationOutlined,
   IdcardOutlined,
 } from '@ant-design/icons';
-// import './Styles/application.css';
+import './Styles/menteeApplication';
 const { Option } = Select;
 
 const initialFormValues = {
