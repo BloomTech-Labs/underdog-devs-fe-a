851c2a1b89f7ffd70410e28a0592510128269939
Brian Bellinger <brianpatrickbellinger@gmail.com>
2022-02-23 23:28:11 -0800

    clean up for rebase

--- src/components/pages/MentorMenteeMatching/MentorMenteeMatching.js
+++ src/components/pages/MentorMenteeMatching/MentorMenteeMatching.js
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from 'react';
 import axiosWithAuth from '../../../utils/axiosWithAuth';
 
 const MentorMenteeMatching = () => {
-<<<<<<< HEAD
-<<<<<<< HEAD
   const [assignments, setAssignments] = useState([]);
 
   useEffect(() => {
@@ -13,67 +11,15 @@ const MentorMenteeMatching = () => {
         .then(res => {
           console.log(res);
           //   setAssignments(res);
-=======
-  const [mentors, setMentors] = useState([]);
-  const [mentees, setMentees] = useState([]);
-=======
-  const [assignments, setAssignments] = useState([]);
->>>>>>> 18bb7ae (cleared redundant code, added correct endpoint)
-
-  useEffect(() => {
-    const getAssignments = () => {
-      axiosWithAuth()
-        .get('/assignments')
-        .then(res => {
-<<<<<<< HEAD
-          setMentors(res);
->>>>>>> bc81dae (matching component started)
-=======
-          console.log(res);
-          //   setAssignments(res);
->>>>>>> 18bb7ae (cleared redundant code, added correct endpoint)
-        })
-        .catch(err => {
-          console.log(err);
         });
     };
-<<<<<<< HEAD
-<<<<<<< HEAD
     getAssignments();
-=======
-    getMentors();
-  }, []);
-
-  useEffect(() => {
-    const getMentees = () => {
-      axiosWithAuth()
-        //put correct endpoint
-        .get('/')
-        .then(res => {
-          setMentees(res);
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    };
-    getMentees();
->>>>>>> bc81dae (matching component started)
-=======
-    getAssignments();
->>>>>>> 18bb7ae (cleared redundant code, added correct endpoint)
   }, []);
 
   return (
     <>
       <h2>Matching</h2>
-<<<<<<< HEAD
-<<<<<<< HEAD
       <p>list here</p>
-=======
->>>>>>> bc81dae (matching component started)
-=======
-      <p>list here</p>
->>>>>>> 18bb7ae (cleared redundant code, added correct endpoint)
     </>
   );
 };
