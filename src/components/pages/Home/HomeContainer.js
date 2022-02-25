c09c068dbd62d3ca084017ea1ad10e901787087f
mrjacobsullivan <mrjacobsullivan@gmail.com>
2022-02-25 12:23:06 -0600

    added useEffects back to the HomeContainer so that previous app functionality is not broken - will be removed further along in the routing refactor process

--- src/components/pages/Home/HomeContainer.js
+++ src/components/pages/Home/HomeContainer.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { useOktaAuth } from '@okta/okta-react';
 
 import Sidebar from '../../common/Sidebar/Sidebar';
 import PendingApproval from '../PendingApproval/PendingApproval';
+import { authenticateUser } from '../../../state/actions/auth/authenticateUser';
+import { getProfile } from '../../../state/actions/userProfile/getProfile';
 
-function HomeContainer({ LoadingComponent, isAuthenticated, userProfile }) {
-  const { authService } = useOktaAuth();
+function HomeContainer({
+  LoadingComponent,
+  isAuthenticated,
+  profile_id,
+  userProfile,
+  dispatch,
+}) {
+  const { authState, authService } = useOktaAuth();
+
+  useEffect(() => {
+    if (Object.keys(userProfile).length === 0) {
+      if (profile_id === null) {
+        if (authState.isPending || authState.isAuthenticated) {
+          dispatch(authenticateUser(authState, authService));
+        }
+      } else {
+        dispatch(getProfile(profile_id));
+      }
+    }
+  }, [userProfile, profile_id, authState, authService, dispatch]);
 
   return (
     <>
@@ -23,6 +43,7 @@ function HomeContainer({ LoadingComponent, isAuthenticated, userProfile }) {
 const mapStateToProps = state => {
   return {
     isAuthenticated: state.user.auth.isAuthenticated,
+    profile_id: state.user.auth.profile_id,
     userProfile: state.user.userProfile,
   };
 };
