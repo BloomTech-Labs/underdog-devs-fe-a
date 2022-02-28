995d4461c9db0f99fd99de4792c097969cbd0b02
Brian Bellinger <brianpatrickbellinger@gmail.com>
2022-02-28 08:44:56 -0800

    added table to matching component

--- src/components/pages/MentorMenteeMatching/MentorMenteeMatching.js
+++ src/components/pages/MentorMenteeMatching/MentorMenteeMatching.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import axiosWithAuth from '../../../utils/axiosWithAuth';
 
+import { Table } from 'antd';
+import { Card } from 'antd';
+import { Tag, Space } from 'antd';
+
 const MentorMenteeMatching = () => {
   const [assignments, setAssignments] = useState([]);
 
@@ -16,10 +20,97 @@ const MentorMenteeMatching = () => {
     getAssignments();
   }, []);
 
+  const columns = [
+    { title: 'Name', dataIndex: 'name', key: 'name' },
+    { title: 'Contact', dataIndex: 'contact', key: 'contact' },
+    {
+      title: 'Status',
+      key: 'tags',
+      dataIndex: 'tags',
+      render: tag => (
+        <Tag color={tag === 'Unmatched' ? 'red' : 'green'}>{tag}</Tag>
+      ),
+    },
+    { title: 'Stack', dataIndex: 'stack', key: 'stack' },
+  ];
+
+  const data = [
+    {
+      key: 1,
+      name: 'Billy Bob',
+      contact: 'email@email.com',
+      tags: 'Matched',
+      stack: 'HTML, JS, CSS',
+      description:
+        'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.',
+    },
+    {
+      key: 2,
+      name: 'Jim Green',
+      contact: '3055555555',
+      tags: 'Unmatched',
+      stack: 'MERN',
+      description:
+        'My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.',
+    },
+    {
+      key: 3,
+      name: 'Gary Bucey',
+      contact: 'email3@email.com',
+      tags: 'Matched',
+      stack: 'HTML, JS, CSS',
+      description: 'This not expandable',
+    },
+    {
+      key: 4,
+      name: 'Joe Black',
+      contact: 'email4@email.com',
+      tags: 'Matched',
+      stack: 'Python',
+      description:
+        'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.',
+    },
+  ];
+
+  let cards = (
+    <div
+      style={{
+        display: 'flex',
+        'flex-direciton': 'row',
+        'justify-content': 'space-evenly',
+      }}
+    >
+      <Card title="Information" style={{ width: '30%' }}>
+        <p>Card content</p>
+        <p>Card content</p>
+        <p>Card content</p>
+      </Card>
+      <Card title="Mentors" style={{ width: '30%' }}>
+        <p>Card content</p>
+        <p>Card content</p>
+        <p>Card content</p>
+      </Card>
+      <Card title="Mentor Infromation" style={{ width: '30%' }}>
+        <p>Card content</p>
+        <p>Card content</p>
+        <p>Card content</p>
+      </Card>
+    </div>
+  );
+
+  // let cards = [card, card2];
+
   return (
     <>
       <h2>Matching</h2>
-      <p>list here</p>
+      <Table
+        columns={columns}
+        expandable={{
+          expandedRowRender: record => <p style={{ margin: 0 }}>{cards}</p>,
+          rowExpandable: record => record.name !== 'Not Expandable',
+        }}
+        dataSource={data}
+      />
     </>
   );
 };
