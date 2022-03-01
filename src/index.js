7239ec6045a1016adbd3243142b28572269ba312
mrjacobsullivan <mrjacobsullivan@gmail.com>
2022-03-01 13:08:44 -0600

    added /support and /meetings private routes
    Co-authored-by: Jacob Sullivan mrjacobsullivan@gmail.com
    Co-authored-by: Felipe Valdenegro fvaldenegro95@gmail.com

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import { Security, LoginCallback, SecureRoute } from '@okta/okta-react';
 import 'antd/dist/antd.less';
 
 import { NotFoundPage } from './components/pages/NotFound';
+// eslint-disable-next-line no-unused-vars
 import { Landing } from './components/pages/LandingPage';
 import { LoginPage } from './components/pages/Login';
 import { HomePage } from './components/pages/Home';
@@ -22,12 +23,8 @@ import Mentee from './components/pages/RoleSignup/Applications/Mentee';
 import Mentor from './components/pages/RoleSignup/Applications/Mentor';
 
 import Navbar from './components/pages/Navbar/Navbar';
-import PendingApproval from './components/pages/PendingApproval/PendingApproval';
 import PendingApplications from './components/pages/PendingApplications/PendingApplication';
-import { Availability } from './components/pages/Availability/Availability';
-import { Schedule } from './components/pages/Schedule/Schedule';
 import { ManageResources } from './components/pages/ManageResources/ManageResources';
-import { MenteesProgress } from './components/pages/MenteesProgress/MenteesProgress';
 import { Profile } from './components/pages/Profile';
 import Dashboard from './components/pages/Dashboard/Dashboard';
 import UserManagement from './components/pages/UserManagement/UserManagement';
@@ -37,7 +34,6 @@ import { Provider } from 'react-redux';
 import rootReducer from './state/reducers';
 import promiseMiddleware from 'redux-promise';
 import thunk from 'redux-thunk';
-import NavBarLanding from './components/pages/NavBarLanding/NavBarLanding';
 
 import PrivateRoute from './components/common/PrivateRoute';
 
@@ -68,11 +64,6 @@ function App() {
     history.push('/login');
   };
 
-  //1 = superadmin
-  //2 = admin
-  //3 = mentor
-  //4 = mentee
-  //5 = pending
   return (
     <Security {...config} onAuthRequired={authHandler}>
       <Navbar />
@@ -83,7 +74,12 @@ function App() {
         <Route path="/apply" exact component={Signup} />
         <Route path="/apply/mentee" component={Mentee} />
         <Route path="/apply/mentor" component={Mentor} />
-        <Route path="/pending" component={PendingApproval} />
+        <Route
+          path="/pending"
+          component={() => (
+            <div>"Application Is Pending" Component goes here</div>
+          )}
+        />
         <Route path="/implicit/callback" component={LoginCallback} />
 
         <PrivateRoute
@@ -112,6 +108,29 @@ function App() {
           redirect="/dashboard"
           allowRoles={[1, 2, 3, 4]}
           component={ManageResources}
+        />
+
+        <PrivateRoute
+          path="/applications"
+          redirect="/dashboard"
+          allowRoles={[1, 2]}
+          component={PendingApplications}
+        />
+
+        <PrivateRoute
+          path="/support"
+          redirect="/dashboard"
+          allowRoles={[1, 2, 3, 4]}
+          component={() => (
+            <div>"View Support Requests" Component goes here</div>
+          )}
+        />
+
+        <PrivateRoute
+          path="/meetings"
+          redirect="/dashboard"
+          allowRoles={[1, 2, 3, 4]}
+          component={() => <div>"View All Meetings" Component goes here</div>}
         />
 
         <SecureRoute
