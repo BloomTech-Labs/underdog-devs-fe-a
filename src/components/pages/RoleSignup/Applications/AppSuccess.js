f40919abd0d1064d621bd9b43af9818106dc7149
[Tristan Harrower] <[harrowertristan@gmail.com]>
2022-03-03 14:38:08 -0500

    styled form success page and added exclamation!

--- src/components/pages/RoleSignup/Applications/AppSuccess.js
+++ src/components/pages/RoleSignup/Applications/AppSuccess.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { Col, Row, Typography } from 'antd';
+const { Title } = Typography;
 
 const AppSuccess = () => {
   return (
-    <div className="success">
-      <h2 className="success-title">Thank you!</h2>
-      <p className="success-desc">
-        Your application was successfully submitted. Underdog Devs will contact
-        you shortly in regards to your application approval.
-      </p>
-    </div>
+    <>
+      <Row align="center" gutter={[16, 16]} style={{ marginTop: '5vh' }}>
+        <Col span={24} align="middle">
+          <Title level={2}>Thank you!</Title>
+        </Col>
+        <Col span={24} align="middle">
+          <Typography>
+            Your application was successfully submitted! Underdog Devs will
+            contact you shortly in regards to your application approval.
+          </Typography>
+        </Col>
+      </Row>
+    </>
   );
 };
 
