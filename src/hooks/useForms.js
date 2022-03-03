383a3f6fae6d69427929afc7270a06773fa8ffd3
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-03-03 14:29:26 -0500

    modify useForms hook to work on forms with select/checkbox

--- src/hooks/useForms.js
+++ src/hooks/useForms.js
@@ -3,13 +3,22 @@ import { useState } from 'react';
 export default function useForms(initialValues) {
   const [formValues, setFormValues] = useState(initialValues);
 
-  const handleChange = e => {
-    const { name, value } = e.target;
-    setFormValues({
-      ...formValues,
-      [name]: value,
-    });
+  const handleChange = (e, type, targetName) => {
+    let name;
+    let value;
+    if (type === 'select') {
+      name = targetName;
+      value = e;
+    } else if (type === 'checkbox') {
+      name = e.target.value;
+      value = e.target.checked;
+    } else {
+      name = e.target.name;
+      value = e.target.value;
+    }
+    setFormValues({ ...formValues, [name]: value });
   };
+
   const clearForm = e => {
     e.preventDefault();
     setFormValues(initialValues);
