b9fb9f963e1c3e721e2b2796547bd120c3c62c1a
jrosado184 <88807540+jrosado184@users.noreply.github.com>
2022-03-14 17:24:10 -0400

    Created fake data to test and use as a placeholder,added the ability to check if a mentor exist in the fake data

--- src/components/pages/MentorMenteeMatching/MentorMenteeInfo.js
+++ src/components/pages/MentorMenteeMatching/MentorMenteeInfo.js
@@ -6,85 +6,157 @@ import './MentorMatching.css';
 const MentorMenteeInfo = () => {
   const [edit, setEdit] = useState(false);
 
-  return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-evenly',
-      }}
-    >
-      <Card title="Information" style={{ width: '30%' }}>
-        <p>Card content</p>
-        <p>Card content</p>
-        <p>Card content</p>
-      </Card>
-      <Card
-        title="Mentors"
-        style={{
-          width: '30%',
-        }}
-        extra={<EditOutlined onClick={() => setEdit(!edit)} />}
-      >
-        {edit ? (
-          <div style={{ width: '80%' }}>
-            <input
-              className={
-                localStorage.getItem('theme') === 'dark'
-                  ? 'mentors-dark'
-                  : 'mentors'
-              }
-              placeholder="Assign Mentor"
-              value="Card Content"
-            />
-          </div>
-        ) : (
-          <div>
-            <p>Card Content</p>
-          </div>
-        )}
+  const fakeData = [
+    {
+      information: {
+        email: 'random@gmail.com',
+        phone: '123-123-123',
+        random: 'random',
+      },
+      Mentors: {
+        first: 'Jimmy Bold',
+      },
+    },
+  ];
 
-        {edit ? (
-          <div style={{ width: '80%', marginTop: '20px' }}>
-            <input
-              className={
-                localStorage.getItem('theme') === 'dark'
-                  ? 'mentors-dark'
-                  : 'mentors'
-              }
-              placeholder="Assign Mentor"
-              value="Card Content"
-            />
-          </div>
-        ) : (
-          <div>
-            <p>Card Content</p>
-          </div>
-        )}
-        {edit ? (
-          <div style={{ width: '80%', marginTop: '20px' }}>
-            <input
-              className={
-                localStorage.getItem('theme') === 'dark'
-                  ? 'mentors-dark'
-                  : 'mentors'
-              }
-              placeholder="Assign Mentor"
-              value="Card Content"
-            />
-          </div>
-        ) : (
-          <div>
-            <p>Card Content</p>
-          </div>
-        )}
-      </Card>
-      <Card title="Mentor Information" style={{ width: '30%' }}>
-        <p>Card content</p>
-        <p>Card content</p>
-        <p>Card content</p>
-      </Card>
-    </div>
+  return (
+    <>
+      {fakeData.map(data => (
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-evenly',
+          }}
+        >
+          <Card title="Information" style={{ width: '30%' }}>
+            <p>{data.information.email}</p>
+            <p>{data.information.phone}</p>
+            <p>{data.information.random}</p>
+          </Card>
+          <Card
+            title="Mentors"
+            style={{
+              width: '30%',
+            }}
+            extra={<EditOutlined onClick={() => setEdit(!edit)} />}
+          >
+            {edit ? (
+              <div style={{ width: '80%' }}>
+                <input
+                  className={
+                    localStorage.getItem('theme') === 'dark'
+                      ? 'mentors-dark'
+                      : 'mentors'
+                  }
+                  placeholder="Assign Mentor"
+                  value={
+                    data.Mentors.first ? data.Mentors.first : 'Assign Mentor'
+                  }
+                />
+              </div>
+            ) : (
+              <div style={{ width: '80%' }}>
+                <p>
+                  {!data.Mentors.first ? (
+                    <input
+                      placeholder="Assign Mentor"
+                      className={
+                        localStorage.getItem('theme') === 'dark'
+                          ? 'mentors-dark'
+                          : 'mentors'
+                      }
+                    />
+                  ) : (
+                    data.Mentors.first
+                  )}
+                </p>
+              </div>
+            )}
+
+            {edit ? (
+              <div style={{ width: '80%', marginTop: '20px' }}>
+                <input
+                  className={
+                    localStorage.getItem('theme') === 'dark'
+                      ? 'mentors-dark'
+                      : 'mentors'
+                  }
+                  placeholder="Assign Mentor"
+                  value={
+                    data.Mentors.second ? data.Mentors.second : 'Assign Mentor'
+                  }
+                />
+              </div>
+            ) : (
+              <div style={{ width: '80%' }}>
+                <p>
+                  {!data.Mentors.second ? (
+                    <input
+                      value={
+                        data.Mentors.second
+                          ? data.Mentors.second
+                          : 'Assign Mentor'
+                      }
+                      placeholder="Assign Mentor"
+                      className={
+                        localStorage.getItem('theme') === 'dark'
+                          ? 'mentors-dark'
+                          : 'mentors'
+                      }
+                    />
+                  ) : (
+                    data.Mentors.second
+                  )}
+                </p>
+              </div>
+            )}
+            {edit ? (
+              <div style={{ width: '80%', marginTop: '20px' }}>
+                <input
+                  className={
+                    localStorage.getItem('theme') === 'dark'
+                      ? 'mentors-dark'
+                      : 'mentors'
+                  }
+                  placeholder="Assign Mentor"
+                  value={
+                    data.Mentors.third ? data.Mentors.third : 'Assign Mentor'
+                  }
+                />
+              </div>
+            ) : (
+              <div style={{ width: '80%' }}>
+                <p>
+                  {!data.Mentors.third ? (
+                    <input
+                      value={
+                        data.Mentors.third
+                          ? data.Mentors.third
+                          : 'Assign Mentor'
+                      }
+                      placeholder="Assign Mentor"
+                      className={
+                        localStorage.getItem('theme') === 'dark'
+                          ? 'mentors-dark'
+                          : 'mentors'
+                      }
+                    />
+                  ) : (
+                    data.Mentors.third
+                  )}
+                </p>
+              </div>
+            )}
+          </Card>
+          <Card title="Mentor Information" style={{ width: '30%' }}>
+            <p>Card content</p>
+            <p>Card content</p>
+            <p>Card content</p>
+          </Card>
+        </div>
+      ))}
+    </>
   );
 };
 
