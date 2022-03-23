3203b683f7e1a169fc5ce989ccf57ec98d45b5ae
Prashish Bhandari <89494812+prashishb@users.noreply.github.com>
2022-03-23 14:11:10 -0400

    used mockapi to populate table with dummy data

--- src/components/common/NotesTable/index.js
+++ src/components/common/NotesTable/index.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Table } from 'antd';
+import { columns } from './NoteUtils';
+import axiosWithAuth from '../../../utils/axiosWithAuth';
 
 const NotesTable = () => {
-  return <Table />;
+  const [data, setData] = useState([]);
+
+  // Dummy data for table
+  useEffect(() => {
+    axiosWithAuth()
+      .get('https://mocki.io/v1/d52d79b6-7bc3-4fdb-8aea-25140c9a1041')
+      .then(res => {
+        setData(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
+  return <Table columns={columns} dataSource={data} />;
 };
 
 export default NotesTable;
