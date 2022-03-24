227a3ddb6c2825446ca5e380051d2c158491a641
Justin Peisker <justinpeisker@gmail.com>
2022-03-24 10:04:37 -0700

    created/scaffolded attendance component

---
+++ src/components/pages/Attendance/attendance.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Attendance = () => {
+  return (
+    <div>
+      <h1> View Mentee Attendance component goes here </h1>
+    </div>
+  );
+};
+export default Attendance;
