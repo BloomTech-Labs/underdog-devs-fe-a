834c78e901651cd69f80937f29d65df0a6b12f16
jrosado184 <88807540+jrosado184@users.noreply.github.com>
2022-03-25 11:13:05 -0400

    made edit button only appear if there is something to edit

--- src/components/pages/MentorMenteeMatching/MentorsCard.js
+++ src/components/pages/MentorMenteeMatching/MentorsCard.js
@@ -2,10 +2,10 @@ import React, { useState } from 'react';
 import { Card, Select } from 'antd';
 import { EditOutlined } from '@ant-design/icons';
 
+const { Option } = Select;
+
 const MentorsCard = ({ data }) => {
   const [edit, setEdit] = useState(false);
-
-  const { Option } = Select;
 
   const [mentors, setMentors] = useState({
     first: '',
@@ -22,7 +22,9 @@ const MentorsCard = ({ data }) => {
           width: '30%',
         }}
         extra={
-          edit ? (
+          !mentors.first && !mentors.second && !mentors.third ? (
+            ''
+          ) : edit ? (
             <p
               onClick={() => setEdit(!edit)}
               style={{
@@ -40,7 +42,7 @@ const MentorsCard = ({ data }) => {
           )
         }
       >
-        {edit || !mentors.first ? (
+        {edit || !mentors.first || mentors.first === 'Assign Mentor' ? (
           <div style={{ width: '80%' }}>
             <Select
               name="first"
@@ -51,6 +53,7 @@ const MentorsCard = ({ data }) => {
               value={mentors.first || 'Assign Mentor'}
             >
               {' '}
+              <Option value="Assign Mentor">Assign Mentor</Option>
               <Option value={data.Mentors.first}>{data.Mentors.first}</Option>
               <Option value={data.Mentors.second}>{data.Mentors.second}</Option>
               <Option value={data.Mentors.third}>{data.Mentors.third}</Option>
@@ -58,11 +61,11 @@ const MentorsCard = ({ data }) => {
           </div>
         ) : (
           <div style={{ width: '80%' }}>
-            <p>{mentors.first}</p>
+            <p style={{ marginLeft: '5px' }}>{mentors.first}</p>
           </div>
         )}
 
-        {edit || !mentors.second ? (
+        {edit || !mentors.second || mentors.second === 'Assign Mentor' ? (
           <div style={{ width: '80%', marginTop: '20px' }}>
             <Select
               name="second"
@@ -72,6 +75,7 @@ const MentorsCard = ({ data }) => {
               optionFilterProp="children"
               value={mentors.second || 'Assign Mentor'}
             >
+              <Option value="Assign Mentor">Assign Mentor</Option>
               <Option value={data.Mentors.first}>{data.Mentors.first}</Option>
               <Option value={data.Mentors.second}>{data.Mentors.second}</Option>
               <Option value={data.Mentors.third}>{data.Mentors.third}</Option>
@@ -79,10 +83,12 @@ const MentorsCard = ({ data }) => {
           </div>
         ) : (
           <div style={{ width: '80%' }}>
-            <p>{mentors.second}</p>
+            <p style={{ marginTop: '15px', marginLeft: '5px' }}>
+              {mentors.second}
+            </p>
           </div>
         )}
-        {edit || !mentors.third ? (
+        {edit || !mentors.third || mentors.third === 'Assign Mentor' ? (
           <div style={{ width: '80%', marginTop: '20px' }}>
             <Select
               name="third"
@@ -93,6 +99,7 @@ const MentorsCard = ({ data }) => {
               value={mentors.third || 'Assign Mentor'}
             >
               {' '}
+              <Option value="Assign Mentor">Assign Mentor</Option>
               <Option value={data.Mentors.first}>{data.Mentors.first}</Option>
               <Option value={data.Mentors.second}>{data.Mentors.second}</Option>
               <Option value={data.Mentors.third}>{data.Mentors.third}</Option>
@@ -100,7 +107,9 @@ const MentorsCard = ({ data }) => {
           </div>
         ) : (
           <div style={{ width: '80%' }}>
-            <p>{mentors.third}</p>
+            <p style={{ marginTop: '15px', marginLeft: '5px' }}>
+              {mentors.third}
+            </p>
           </div>
         )}
       </Card>
