4f0da984949e45ad9b727ffbcc3ec7e16160baf5
Brett Smith <brettsmith212@gmail.com>
2022-04-13 14:25:10 -0700

    refactored userManagement | updates page onclick with admin update

--- src/components/pages/UserManagement/UserManagement.js
+++ src/components/pages/UserManagement/UserManagement.js
@@ -4,98 +4,100 @@ import axiosWithAuth from '../../../utils/axiosWithAuth';
 import { Table, Button } from 'antd';
 import NotesTable from '../../common/NotesTable';
 
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-    defaultSortOrder: 'descend',
-    sorter: (a, b) => a.name - b.name,
-  },
-  {
-    title: 'Role',
-    dataIndex: 'role',
-    key: 'role',
-    filters: [
-      {
-        text: 'superAdmin',
-        value: 'superAdmin',
-      },
-      {
-        text: 'admin',
-        value: 'admin',
-      },
-      {
-        text: 'mentor',
-        value: 'mentor',
-      },
-      {
-        text: 'mentee',
-        value: 'mentee',
-      },
-    ],
-    onFilter: (value, record) => record.role.includes(value),
-  },
-  {
-    title: 'Email',
-    dataIndex: 'email',
-    key: 'date',
-    defaultSortOrder: 'descend',
-    sorter: (a, b) => a.date - b.date,
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    render: (text, record) => (
-      <Button onClick={() => updateToAdmin(record)}>Update to Admin</Button>
-    ),
-  },
-];
-
-function updateToAdmin(record) {
-  console.log(record);
-  const requestBody = {
-    role_id: 2,
-  };
-  axiosWithAuth()
-    .put(`/profile/${record.key}`, requestBody)
-    .then(res => console.log(res))
-    .catch(err => console.error(err));
-}
-
 const UserManagement = () => {
   const [accounts, setAccounts] = useState([]);
+  const [updatedProfile, setUpdatedProfile] = useState();
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      defaultSortOrder: 'descend',
+      sorter: (a, b) => a.name - b.name,
+    },
+    {
+      title: 'Role',
+      dataIndex: 'role',
+      key: 'role',
+      filters: [
+        {
+          text: 'superAdmin',
+          value: 'superAdmin',
+        },
+        {
+          text: 'admin',
+          value: 'admin',
+        },
+        {
+          text: 'mentor',
+          value: 'mentor',
+        },
+        {
+          text: 'mentee',
+          value: 'mentee',
+        },
+      ],
+      onFilter: (value, record) => record.role.includes(value),
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'date',
+      defaultSortOrder: 'descend',
+      sorter: (a, b) => a.date - b.date,
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (text, record) => (
+        <Button onClick={() => updateToAdmin(record)}>Update to Admin</Button>
+      ),
+    },
+  ];
+
+  function updateToAdmin(record) {
+    const requestBody = {
+      role_id: 2,
+    };
+    axiosWithAuth()
+      .put(`/profile/${record.key}`, requestBody)
+      .then(res => setUpdatedProfile(res))
+      .catch(err => console.error(err));
+  }
+
+  const getAccounts = () => {
+    axiosWithAuth()
+      .get('/profile')
+      .then(res => {
+        setAccounts(
+          res.data.map(row => ({
+            key: row.profile_id,
+            name: row.first_name + ' ' + row.last_name,
+            role:
+              row.role_id === 1
+                ? 'superAdmin'
+                : row.role_id === 2
+                ? 'admin'
+                : row.role_id === 3
+                ? 'mentor'
+                : row.role_id === 4
+                ? 'mentee'
+                : 'pending',
+            email: row.email,
+            notes: 'this is a note',
+          }))
+        );
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
   useEffect(() => {
-    const getAccounts = () => {
-      axiosWithAuth()
-        .get('/profile')
-        .then(res => {
-          setAccounts(
-            res.data.map(row => ({
-              key: row.profile_id,
-              name: row.first_name + ' ' + row.last_name,
-              role:
-                row.role_id === 1
-                  ? 'superAdmin'
-                  : row.role_id === 2
-                  ? 'admin'
-                  : row.role_id === 3
-                  ? 'mentor'
-                  : row.role_id === 4
-                  ? 'mentee'
-                  : 'pending',
-              email: row.email,
-              notes: 'this is a note',
-            }))
-          );
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    };
     getAccounts();
-  }, []);
+  }, [updatedProfile]);
+
   return (
     <>
       <h2>Manage Users</h2>
