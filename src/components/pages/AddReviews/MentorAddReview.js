b714b775e274d93a98c6261284604d3e5dd776bd
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2022-04-21 10:17:57 -0400

    fleshed out initial MentorAddReview items to mimic MenteeAddReview - update specifics at a later time

--- src/components/pages/AddReviews/MentorAddReview.js
+++ src/components/pages/AddReviews/MentorAddReview.js
@@ -18,59 +18,58 @@ const formItemLayout = {
 };
 
 const MentorAddReview = () => {
-  //   const [formValues, setFormValues] = useState({
-  //     mentor_name: '',
-  //     review: '',
-  //   });
+  const [formValues, setFormValues] = useState({
+    mentee_name: '',
+    review: '',
+  });
 
-  //   const handleChange = e => {
-  //     setFormValues({
-  //       ...formValues,
-  //       [e.target.name]: e.target.value,
-  //     });
-  //   };
+  const handleChange = e => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-  //   const handleSubmit = e => {
-  //     e.preventDefault()
-  //       // axiosWithAuth()
-  //       .post(`/reviews`, formValues)
-  //       .then(res => {})
-  //       .catch(err => console.log({ err }));
-  //   };
+  const handleSubmit = e => {
+    e.preventDefault()
+      // axiosWithAuth()
+      .post(`/reviews`, formValues)
+      .then(res => {})
+      .catch(err => console.log({ err }));
+  };
 
   return (
-    // <Form
-    //   {...formItemLayout}
-    //   style={{ display: 'flex', flexDirection: 'column' }}
-    // >
-    //   <p style={{ fontWeight: 'bold', paddingLeft: '1.3rem' }}>
-    //     Please fill out a review
-    //   </p>
-    //   <Form.Item
-    //     name="mentor_name"
-    //     label="Mentor Name"
-    //     value={formValues.mentor_name}
-    //     onChange={handleChange}
-    //     rules={[{ required: true, message: 'Please input a mentor name' }]}
-    //   >
-    //     <Input />
-    //   </Form.Item>
+    <Form
+      {...formItemLayout}
+      style={{ display: 'flex', flexDirection: 'column' }}
+    >
+      <p style={{ fontWeight: 'bold', paddingLeft: '1.3rem' }}>
+        Please fill out a review
+      </p>
+      <Form.Item
+        name="mentee_name"
+        label="Mentee Name"
+        value={formValues.mentee_name}
+        onChange={handleChange}
+        rules={[{ required: true, message: 'Please input a mentee name' }]}
+      >
+        <Input />
+      </Form.Item>
 
-    //   <Form.Item
-    //     name="mentor_review"
-    //     label="Mentor Review"
-    //     value={formValues.mentor_review}
-    //     onChange={handleChange}
-    //     rules={[{ required: true, message: 'Please input a mentor review' }]}
-    //   >
-    //     <TextArea />
-    //   </Form.Item>
+      <Form.Item
+        name="mentee_review"
+        label="Mentee Review"
+        value={formValues.mentee_review}
+        onChange={handleChange}
+        rules={[{ required: true, message: 'Please input a mentee review' }]}
+      >
+        <TextArea />
+      </Form.Item>
 
-    //   <Form.Item style={{ alignSelf: 'center' }}>
-    //     <Button onClick={handleSubmit}>Submit review</Button>
-    //   </Form.Item>
-    // </Form>
-    <div></div>
+      <Form.Item style={{ alignSelf: 'center' }}>
+        <Button onClick={handleSubmit}>Submit review</Button>
+      </Form.Item>
+    </Form>
   );
 };
 
