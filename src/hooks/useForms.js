9aa9a79c42a6545d0d12637707a33dc4fd69c8a1
Benedict Schurwanz <19333755+benedictschurwanz@users.noreply.github.com>
2022-04-28 01:54:05 -0500

    refactor useForms hook, adding handleSelect function to handle ant design Select components

--- src/hooks/useForms.js
+++ src/hooks/useForms.js
@@ -3,25 +3,11 @@ import { useState } from 'react';
 export default function useForms(initialValues) {
   const [formValues, setFormValues] = useState(initialValues);
 
-  const handleChange = (e, type, targetName) => {
-    let name;
-    let value;
-    if (type === 'select') {
-      name = targetName;
-      value = e;
-    } else if (type === 'checkbox') {
-      name = e.target.value;
-      value = e.target.checked;
-    } else if (type === 'radio') {
-      name = e.target.name;
-      value = e.target.value;
-    } else if (e.target.type === 'text') {
-      name = e.target.id;
-      value = e.target.value;
-    } else {
-      name = e.target.name;
-      value = e.target.value;
-    }
+  const handleChange = e => {
+    setFormValues({ ...formValues, [e.target.id]: e.target.value });
+  };
+
+  const handleSelect = ({ name, value }) => {
     setFormValues({ ...formValues, [name]: value });
   };
 
@@ -29,5 +15,5 @@ export default function useForms(initialValues) {
     setFormValues(initialValues);
   };
 
-  return [formValues, handleChange, setFormValues, clearForm];
+  return [formValues, setFormValues, handleChange, handleSelect];
 }
