a6c462483d2982c0e2888ab48437909a71842a45
Benedict Schurwanz <19333755+benedictschurwanz@users.noreply.github.com>
2022-04-28 15:22:15 -0500

    add handler functions for Radio and Checkbox inputs, return through handlers object

--- src/hooks/useForms.js
+++ src/hooks/useForms.js
@@ -5,15 +5,29 @@ export default function useForms(initialValues) {
 
   const handleChange = e => {
     setFormValues({ ...formValues, [e.target.id]: e.target.value });
+    console.log('formValues: ', formValues);
+    console.log('e.target.value: ', e.target.value);
+  };
+
+  const handleRadio = e => {
+    console.log('handleRadio arg: ', e);
+  };
+
+  const handleCheckbox = e => {
+    console.log('handleCheckbox arg: ', e);
   };
 
   const handleSelect = ({ name, value }) => {
     setFormValues({ ...formValues, [name]: value });
+    console.log('formValues: ', formValues);
   };
 
-  const clearForm = () => {
-    setFormValues(initialValues);
+  const handlers = {
+    handleChange,
+    handleRadio,
+    handleCheckbox,
+    handleSelect,
   };
 
-  return [formValues, setFormValues, handleChange, handleSelect];
+  return { formValues, setFormValues, handlers };
 }
