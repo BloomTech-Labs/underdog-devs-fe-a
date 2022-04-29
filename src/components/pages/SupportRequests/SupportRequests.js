05c7f706d1237a05fbe6554e2c03c5a323542e89
Austin Marrazza <56844892+amarrazza@users.noreply.github.com>
2022-04-28 17:32:08 -0700

    added ant design table

--- src/components/pages/SupportRequests/SupportRequests.js
+++ src/components/pages/SupportRequests/SupportRequests.js
@@ -1,7 +1,47 @@
 import React from 'react';
+import { Statistic, Row, Col, Table } from 'antd';
+
+const columns = [
+  {
+    title: 'Ticket ID',
+    dataIndex: 'ticketID',
+    sorter: (a, b) => a.ticketID - b.ticketID,
+    sortDirections: ['descend'],
+  },
+  {
+    title: 'Message',
+    dataIndex: 'message',
+  },
+  {
+    title: 'Date Submitted',
+    dataIndex: 'dateSubmitted',
+    defaultSortOrder: 'descend',
+    sorter: (a, b) => a.dateSubmitted < b.dateSubmitted,
+  },
+];
+
+function onChange(pagination, filters, sorter, extra) {
+  console.log('params', pagination, filters, sorter, extra);
+}
 
 const SupportRequests = () => {
-  return <div>hihi</div>;
+  const data = [];
+
+  return (
+    <div className="dashboard-container">
+      <h2>Support Requests</h2>
+      <div className="dashboard-statistics">
+        <Row gutter={16}>
+          <Col span={5}>
+            <Statistic title="# of Requests" value={99} />
+          </Col>
+        </Row>
+      </div>
+      <div className="dashboard-table-container">
+        <Table dataSource={data} columns={columns} onChange={onChange} />
+      </div>
+    </div>
+  );
 };
 
 export default SupportRequests;
