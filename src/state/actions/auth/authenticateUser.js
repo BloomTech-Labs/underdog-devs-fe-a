b3e4eeb415bd0d941bda3ebbb8d3efc4973c6d8e
Mina <minasoha@yahoo.com>
2022-05-04 15:34:53 -0400

    working on the authenticateUser

--- src/state/actions/auth/authenticateUser.js
+++ src/state/actions/auth/authenticateUser.js
@@ -3,12 +3,13 @@ import { setFetchEnd } from '../lifecycle/setFetchEnd';
 import { setFetchError } from '../errors/setFetchError';
 import { setProfileId } from './setProfileId';
 import { setIsAuthenticated } from './setIsAuthenticated';
+import axiosWithAuth from './../../../utils/axiosWithAuth';
 
 export const authenticateUser = (authState, authService) => dispatch => {
   if (authState.isAuthenticated) {
     dispatch(setFetchStart());
-    authService
-      .getUser()
+    axiosWithAuth()
+      .get('/profile')
       .then(parsedJWT => {
         const profile_id = parsedJWT.sub; // sub = profile_id from Okta JWT
         dispatch(setProfileId(profile_id));
