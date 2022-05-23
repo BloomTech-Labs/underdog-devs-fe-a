6a0277e5a6f5ea0a48f3e517c536ba9b821815c6
Mina Soha <87757115+minasoha@users.noreply.github.com>
2022-05-23 14:58:20 -0400

    reverting unintended commit

--- src/state/actions/auth/authenticateUser.js
+++ src/state/actions/auth/authenticateUser.js
@@ -3,13 +3,12 @@ import { setFetchEnd } from '../lifecycle/setFetchEnd';
 import { setFetchError } from '../errors/setFetchError';
 import { setProfileId } from './setProfileId';
 import { setIsAuthenticated } from './setIsAuthenticated';
-import axiosWithAuth from './../../../utils/axiosWithAuth';
 
 export const authenticateUser = (authState, authService) => dispatch => {
   if (authState.isAuthenticated) {
     dispatch(setFetchStart());
-    axiosWithAuth()
-      .get('/profile')
+    authService
+      .getUser()
       .then(parsedJWT => {
         const profile_id = parsedJWT.sub; // sub = profile_id from Okta JWT
         dispatch(setProfileId(profile_id));
