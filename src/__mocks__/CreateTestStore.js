4cd351f224f2696e80095ac24c661842e89658b8
Mina Soha <87757115+minasoha@users.noreply.github.com>
2022-06-02 16:45:43 -0400

    installed redux toolkit and refactored CreateTestStore.js file

--- src/__mocks__/CreateTestStore.js
+++ src/__mocks__/CreateTestStore.js
@@ -1,10 +1,11 @@
-import { createStore, applyMiddleware } from 'redux';
+import { applyMiddleware } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from '../state/reducers/index';
 import promiseMiddleware from 'redux-promise';
 import thunk from 'redux-thunk';
 
 export default function createTestStore() {
-  const store = createStore(
+  const store = configureStore(
     rootReducer,
     applyMiddleware(thunk, promiseMiddleware)
   );
