2022c990532d6fc9c11b322d81274ddc4ea814d5
DarkedgeTH <darkedgeth@gmail.com>
2022-08-05 15:48:01 -0400

    added skip to test per review comments

--- src/__tests__/MentorDashboard.test.js
+++ src/__tests__/MentorDashboard.test.js
@@ -1,3 +1,3 @@
-test('Home Container no longer exists, this entire test needs to be revisited/refactored or deleted. It was originally for the Mentor Dashboard Test', () => {
+test.skip('Home Container no longer exists, this entire test needs to be revisited/refactored or deleted. It was originally for the Mentor Dashboard Test', () => {
   console.log('Revisit this test');
 });
