025f26c00d8adb375c550b6e95e8f52c047fd4dc
Farhaan Nishtar <farhaanmnishtar99@gmail.com>
2022-08-12 12:46:23 -0400

    Created button to redirect to landing page

--- src/components/pages/RoleSignup/Applications/AppSuccess.js
+++ src/components/pages/RoleSignup/Applications/AppSuccess.js
@@ -5,6 +5,7 @@ import { getProfile } from '../../../../state/actions/userProfile/getProfile';
 import axiosWithAuth from '../../../../utils/axiosWithAuth';
 import { useOktaAuth } from '@okta/okta-react';
 import { Link } from 'react-router-dom';
+import LandingPage from '../../LandingPage/LandingPage';
 const { Title } = Typography;
 
 const AppSuccess = ({ isAuthenticated, userProfile, getProfile }) => {
@@ -37,28 +38,22 @@ const AppSuccess = ({ isAuthenticated, userProfile, getProfile }) => {
   }, [user.profile_id]);
 
   return (
-    true && (
-      <>
-        <Row align="center" gutter={[16, 16]} style={{ marginTop: '5vh' }}>
-          <Col span={24} align="middle">
-            <Title level={2}>Thank you {user.first_name}!</Title>
-          </Col>
-          <Col span={24} align="middle">
-            <Typography>
-              Your application was successfully submitted! Someone at Underdog
-              Devs will contact you shortly in regards to your application
-              approval.
-              {
-                // Potential other text: “Your application has been received, keep an eye on your email, as we'll email you when your application has been approved.”
-              }
-            </Typography>
-          </Col>
-          {/* <Link to="/login">
-            <Button className="button">Logout</Button>
-          </Link> */}
-        </Row>
-      </>
-    )
+    <>
+      <Row align="center" gutter={[16, 16]} style={{ marginTop: '5vh' }}>
+        <Col span={24} align="middle">
+          <Title level={2}>Thank you {user.first_name}!</Title>
+        </Col>
+        <Col span={24} align="middle">
+          <Typography>
+            Your application has been received, keep an eye on your email, as
+            we'll email you when your application has been approved.
+          </Typography>
+        </Col>
+        <Link to="/landing">
+          <Button className="button">Home</Button>
+        </Link>
+      </Row>
+    </>
   );
 };
 
