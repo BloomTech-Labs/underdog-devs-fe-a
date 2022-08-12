c851ac0f9b7ff6debc3d5e1e1050aa03e2d28ea4
Farhaan Nishtar <farhaanmnishtar99@gmail.com>
2022-08-12 15:56:35 -0400

    Removed unneccessary console.logs and a useEffect hook in AppSuccess.js

--- src/components/pages/RoleSignup/Applications/AppSuccess.js
+++ src/components/pages/RoleSignup/Applications/AppSuccess.js
@@ -3,39 +3,19 @@ import { Col, Row, Typography, Button } from 'antd';
 import { connect } from 'react-redux';
 import { getProfile } from '../../../../state/actions/userProfile/getProfile';
 import axiosWithAuth from '../../../../utils/axiosWithAuth';
-import { useOktaAuth } from '@okta/okta-react';
 import { Link } from 'react-router-dom';
-import LandingPage from '../../LandingPage/LandingPage';
 const { Title } = Typography;
 
-const AppSuccess = ({ isAuthenticated, userProfile, getProfile }) => {
+const AppSuccess = ({ isAuthenticated, getProfile }) => {
   const [user, setUser] = useState({});
-  console.log(
-    `Welcome ${user.first_name}. Your profile id is ${user.profile_id}`
-  );
   useEffect(() => {
     axiosWithAuth()
       .get(`/profile/current_user_profile/`)
       .then(user => {
         setUser(user.data);
         getProfile(user.data.profile_id);
-        console.log('user: ', user);
       });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isAuthenticated]);
-
-  useEffect(() => {
-    console.log('Inisde second useEffect??');
-    axiosWithAuth()
-      .get(`/application/profileId/:${user.profile_id}`)
-      .then(res => {
-        console.log('res: ', res);
-      })
-      .catch(err => {
-        console.log('');
-      });
-    // eslint-disable-next
-  }, [user.profile_id]);
 
   return (
     <>
