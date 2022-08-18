2bd15b311364a143b0894744104ced79c60913f0
John Ruddock <john.jxr@pm.me>
2022-08-18 15:30:50 -0400

    Creates content for toggle switch popover as a component
    Co-authored-by: Charles dagoclmt@gmail.com

---
+++ src/components/pages/Navbar/NavbarFeatures/MentorPopover.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MentorPopover = () => (
+  <>
+    <p>
+      Please indicate if you are <br />
+      available to take on new mentees. <br />
+      This is an important part of our matching <br />
+      process.
+    </p>
+  </>
+);
+export default MentorPopover;
