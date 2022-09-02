10791613f876463d5cde78f1d750b8d9c4c60e35
enhopkin@gmail.com <enhopkin@gmail.com>
2022-09-01 21:42:56 -0600

    completed implementation for PrivateRoute with redirect to auth0 login page

--- src/components/common/PrivateRoute.js
+++ src/components/common/PrivateRoute.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { withAuthenticationRequired } from '@auth0/auth0-react';
 import Sidebar from './Sidebar/Sidebar';
+import { Spin } from 'antd';
 
 const PrivateRoute = ({ component, path, ...args }) => (
   <Sidebar>
     <Route
       path={path}
-      /* NOTE: this is commented out for current functionality, but is a template for building auth0 into the app*/
-      // component={withAuthenticationRequired(component({ ...args }), {
-      //   onRedirecting: () => <div>loading </div>,
-      // })}
-      component={() => component({ ...args })}
+      component={withAuthenticationRequired(component, {
+        onRedirecting: () => <Spin />,
+      })}
+      {...args}
     />
   </Sidebar>
 );
