65029d351a724ed6563ec863b0a411a26d4c0636
enhopkin@gmail.com <enhopkin@gmail.com>
2022-09-01 21:37:06 -0600

    removed commented out code in useAxiosWithAuth0.js

--- src/hooks/useAxiosWithAuth0.js
+++ src/hooks/useAxiosWithAuth0.js
@@ -1,32 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import axios from 'axios';
 import { useAuth0 } from '@auth0/auth0-react';
 
-// export const useAxiosWithAuth = (url, options = {}) => {
-//   const { getAccessTokenSilently } = useAuth0();
-//   const [state, setState] = useState({
-//     error: null,
-//     loading: true,
-//     data: null,
-//   });
-//   const [refreshIndex, setRefreshIndex] = useState(0);
-
-//   useEffect(() => {
-//     (async () => {
-//       try {
-//         const { audience, scope, ...fetchOptions } = options;
-//         const accessToken = await getAccessTokenSilently({ audience, scope });
-//         const res = await
-//       } catch(err) {
-
-//       }
-//     })();
-//   }, [refreshIndex])
-// }
-
 export default function useAxiosWithAuth0() {
   const { getAccessTokenSilently } = useAuth0();
-  // const token = useAuth0().getAccessTokenSilently();
 
   const axiosWithAuth = token =>
     axios.create({
@@ -42,7 +19,7 @@ export default function useAxiosWithAuth0() {
       console.log('token:', token);
       axiosWithAuth(token);
     })();
-  }, []);
+  }, [getAccessTokenSilently]);
 
   return {
     axiosWithAuth,
