40c38c30cf9189812a14a131bf661f8670e33180
enhopkin@gmail.com <enhopkin@gmail.com>
2022-09-02 11:43:14 -0600

    added note to explain the use of the useAxiosWithAuth0() hook throughout the application

--- src/hooks/useAxiosWithAuth0.js
+++ src/hooks/useAxiosWithAuth0.js
@@ -2,6 +2,13 @@ import { useEffect } from 'react';
 import axios from 'axios';
 import { useAuth0 } from '@auth0/auth0-react';
 
+/*
+NOTE:
+React requires that we can only call hooks in function components or within other hooks. As axiosWithAuth is simply a helper function we've built a useAxiosWithAuth0() hook in which we've called both axiosWithAuth AND useAuth0().
+axiosWithAuth serves the same function as it typically does. useAuth0() gives access to getAccessTokenSilently() for authentication/authorization purposes. 
+This hook below is exported and used throughout the app in place of axiosWithAuth.
+THIS useAxiosWithAuth0 HOOK SIMPLY SERVES TO HELP CONNECT THE AXIOS CALL AND THE AUTHENTICATION.
+*/
 export default function useAxiosWithAuth0() {
   const { getAccessTokenSilently } = useAuth0();
 
