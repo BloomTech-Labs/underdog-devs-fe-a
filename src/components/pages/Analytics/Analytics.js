9b5db88d8e2acc27968bfa189533727b72f51676
MaxT6 <maximiliant6@gmail.com>
2022-09-13 11:34:27 -0400

    created new private route for Analytics page

--- src/components/pages/Analytics/Analytics.js
+++ src/components/pages/Analytics/Analytics.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 function Analytics() {
-  return <></>;
+  return (
+    <>
+      <div className="header-container">
+        <h1> Analytics</h1>
+      </div>
+    </>
+  );
 }
 
 export default Analytics;
