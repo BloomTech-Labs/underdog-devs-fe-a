caca4a3ae92c9f4aef96135ab713e5b75b3723f8
ashurik522 <ashurik522@gmail.com>
2022-10-12 11:29:52 -0400

    added columns to table

--- src/components/pages/MyMentees/MyMentees.js
+++ src/components/pages/MyMentees/MyMentees.js
@@ -1,10 +1,24 @@
 import React, { useState } from 'react';
+import { Table } from 'antd';
+
+const columns = [
+  {
+    title: 'Name',
+  },
+  {
+    title: 'Info',
+  },
+  {
+    title: 'Profile',
+  },
+];
 
 const MyMentees = props => {
   return (
     <>
       <div>
         <h2>My Mentees</h2>
+        <Table columns={columns} />
       </div>
     </>
   );
