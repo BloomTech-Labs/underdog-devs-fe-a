902694d93c24ef70041e80eacbd76f06857d4080
James <heartandkarma@gmail.com>
2022-10-19 14:12:38 -0700

    created state for the graph object. Lorem has been replaced with the newly recieved description. Also cleaned the file up a bit.

--- src/components/pages/Analytics/TechStackGraph.jsx
+++ src/components/pages/Analytics/TechStackGraph.jsx
@@ -1,33 +1,26 @@
 import axios from 'axios';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import embed from 'vega-embed';
+//vega-embed allows us to take a graph object, and display it on the ui.
+// for more information visit https://github.com/vega/vega-embed
+
 export default function TechStackGraph() {
+  const [graphData, setGraphData] = useState({ graph: {}, description: '' });
   // let url = `${process.env.REACT_APP_DS_API_URL}/graph/tech-stack-by-role`;
-  let url =
+  const url =
     'http://underdog-devs-ds-a-dev.us-east-1.elasticbeanstalk.com/graph/tech-stack-by-role';
-  // embed is an asynchronous api call, resolves in a promise
-  // for more information visit https://github.com/vega/vega-embed
 
   useEffect(() => {
     axios.get(`${url}`).then(function (res) {
-      const graphImage = res.data.graph;
-      var graphDescription = res.data.description;
-      embed('#vis', graphImage);
+      setGraphData(res.data);
     });
-  });
+  }, url);
+  embed('#vis', graphData.graph);
 
   return (
     <>
       <div id="vis"></div>
-      <p className="techStackGraphInfo">
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum."
-      </p>
+      <p className="techStackGraphInfo">{graphData.description}</p>
     </>
   );
 }
