27335b4e8b355f75fda938537420a710e8d39534
James <heartandkarma@gmail.com>
2022-10-20 14:13:45 -0700

    Fixed the hardcoded url as suggested in the pr comments. I had to change the .env file from DS_API_URL, to REACT_APP_DS_API_URL... This may be needed to be updated on the Enviroment Vars page on the notion product page.

--- src/components/pages/Analytics/TechStackGraph.jsx
+++ src/components/pages/Analytics/TechStackGraph.jsx
@@ -3,15 +3,11 @@ import axios from 'axios';
 import embed from 'vega-embed';
 //vega-embed allows us to take a graph object, and display it on the ui.
 // for more information visit https://github.com/vega/vega-embed
-
 export default function TechStackGraph() {
   const [graphData, setGraphData] = useState({ graph: {}, description: '' });
-  // let url = `${process.env.REACT_APP_DS_API_URL}/graph/tech-stack-by-role`;
-  const url =
-    'http://underdog-devs-ds-a-dev.us-east-1.elasticbeanstalk.com/graph/tech-stack-by-role';
-
+  let url = `${process.env.REACT_APP_DS_API_URL}/graph/tech-stack-by-role`;
   useEffect(() => {
-    axios.get(`${url}`).then(function (res) {
+    axios.get(url).then(function (res) {
       setGraphData(res.data);
     });
   }, []);
