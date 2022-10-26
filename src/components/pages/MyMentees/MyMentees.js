47221c5bf6efe62cd8f3bdf410bdaeacd030f464
ashurik522 <ashurik522@gmail.com>
2022-10-26 12:05:06 -0400

    removed MyMentors page to use on another branch. Set up State with dummy data and implemented axios call

--- src/components/pages/MyMentees/MyMentees.js
+++ src/components/pages/MyMentees/MyMentees.js
@@ -1,56 +1,60 @@
 import React, { useEffect, useState } from 'react';
 import useAxiosWithAuth0 from '../../../hooks/useAxiosWithAuth0';
 import { Table } from 'antd';
+import { connect } from 'react-redux';
+import userReducer from '../../../state/reducers/userReducer';
+import { setProfileId } from '../../../state/actions/auth/setProfileId';
 
 const MyMentees = props => {
   const columns = [
     {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
+      title: 'First Name',
+      dataIndex: 'first_name',
+      key: 'first_name',
+    },
+    {
+      title: 'last Name',
+      dataIndex: 'last_name',
+      key: 'last_name',
     },
     {
       title: 'Email',
       dataIndex: 'email',
       key: 'email',
     },
-    {
-      title: 'Profile',
-      dataIndex: 'profile',
-      key: 'profile',
-    },
   ];
 
-  const [role, setRole] = useState('');
   const { axiosWithAuth } = useAxiosWithAuth0();
+  const { role, profile_id } = props;
+  const [data, setData] = useState([]);
 
-  const getMentor = () => {
-    const { profileId, role } = props;
-
+  useEffect(() => {
     axiosWithAuth()
-      .get(`/assignments/${role}/${profileId}`)
+      .get(`/assignments/${role}/${profile_id}`)
       .then(res => {
-        console.log(res.data);
+        setData(res.data);
       })
       .catch(err => {
         console.error(err);
       });
-  };
-
-  useEffect(() => {
-    getMentor();
   }, []);
-
   return (
     <>
       <div>
         <h2>My Mentees</h2>
         <div style={{ wordWrap: 'break-word', wordBreak: 'break-word' }}>
-          <Table columns={columns} />
+          <Table columns={columns} dataSource={data} key={data.profile_id} />
         </div>
       </div>
     </>
   );
 };
 
-export default MyMentees;
+const mapStateToProps = state => {
+  return {
+    profile_id: '5b36a8d6-dd73-4c11-9c42-d4c086015db2',
+    role: 'mentee',
+  };
+};
+
+export default connect(mapStateToProps)(MyMentees);
