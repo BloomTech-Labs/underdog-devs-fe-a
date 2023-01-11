f678eadfcfc07bb92c43084d1850cbcd92cb0ba5
Branden Jones <branden.wayne.jones@gmail.com>
2023-01-11 14:02:08 -0500

    dashboard uses switch
    Co-authored-by:
    jason decker
    jdecker117@gmail.com
    
    Co-authored-by:
    jess williams
    jesswillcode@gmail.com
    
    Co-authored-by:
    adan rodriguez
    arodriguez133@icloud.com
    
    Co-authored-by:
    
    Sean-John Ubele-Knuckles
    seanjohnreedubeleknuckles@gmail.com

--- src/components/pages/Dashboard/Dashboard.js
+++ src/components/pages/Dashboard/Dashboard.js
@@ -1,46 +1,71 @@
-import React, { useEffect, useState } from 'react';
-import useAxiosWithAuth0 from '../../../hooks/useAxiosWithAuth0';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 import { Statistic, Row, Col, Table } from 'antd';
+import { setCurrentUser } from '../../../state/actions/userProfile/setCurrentUser';
 // TODO: update page styling and functionality, see wireframes/NewDesignProposition/AdminWireframes/Dashboard
 
-/**
- * Khaleel Musleh
- * Created Dashboard Compoenent and set it to display the user role, this component is for future use in displaying dashboard based on user role, each dashboard will be set differently
- * based on user role.
- */
+const Dashboard = props => {
+  const { role_id } = props.user.currentUser;
 
-const Dashboard = () => {
-  const [role, setRole] = useState([]);
-  const axiosWithAuth = useAxiosWithAuth0();
+  useEffect(
+    dispatch => {
+      if (!role_id) {
+        dispatch(setCurrentUser());
+      }
+    },
+    [role_id]
+  );
 
-  const getAccounts = () => {
-    axiosWithAuth
-      .get('/profile/current_user_profile/')
-      .then(res => {
-        setRole(
-          res.data.role_id === 1
-            ? 'Super Admin'
-            : res.data.role_id === 2
-            ? 'Admin'
-            : res.data.role_id === 3
-            ? 'Mentor'
-            : res.data.role_id === 4
-            ? 'Mentee'
-            : res.data.role_id === 5
-            ? 'Developer'
-            : 'pending'
-        );
-      })
-      .catch(err => {
-        console.error(err);
-      });
-  };
-
-  useEffect(() => {
-    getAccounts();
-  }, []);
-
-  return <div>{`Landing page for ${role}`}</div>;
+  switch (role_id) {
+    case 1: // SuperAdmin
+      return <AdminDashboard path={'/'} />; //eslint-disable-line
+    case 2: // Admin
+      return <AdminDashboard path={'/'} />; //eslint-disable-line
+    case 3: // Mentors
+      return <MentorDashboard path={'/'} />; //eslint-disable-line
+    case 4: // Mentees
+      return <MenteeDashboard path={'/'} />; //eslint-disable-line
+    default:
+      return console.log('Role_ID not Found');
+  }
 };
 
-export default Dashboard;
+const mapStateToProps = state => {
+  return { user: state.userReducer };
+};
+
+// const Dashboard = () => {
+//   const [role, setRole] = useState([]);
+//   const axiosWithAuth = useAxiosWithAuth0();
+
+//   const getAccounts = () => {
+//     axiosWithAuth
+//       .get('/profile/current_user_profile/')
+//       .then(res => {
+//         setRole(
+//           res.data.role_id === 1
+//             ? 'Super Admin'
+//             : res.data.role_id === 2
+//             ? 'Admin'
+//             : res.data.role_id === 3
+//             ? 'Mentor'
+//             : res.data.role_id === 4
+//             ? 'Mentee'
+//             : res.data.role_id === 5
+//             ? 'Developer'
+//             : 'pending'
+//         );
+//       })
+//       .catch(err => {
+//         console.error(err);
+//       });
+//   };
+
+// useEffect(() => {
+//   getAccounts();
+// }, []);
+
+// return <div>{`Landing page for ${role}`}</div>;
+// };
+
+export default connect(mapStateToProps)(Dashboard);
