49fe7314eef05783fa6eb4012ec41897ab66e40d
Branden Jones <branden.wayne.jones@gmail.com>
2023-01-18 16:43:42 -0500

    changed auth0Provider to route specific dashboard

--- src/auth/auth0ProviderWithHistory.js
+++ src/auth/auth0ProviderWithHistory.js
@@ -16,7 +16,7 @@ const Auth0ProviderWithHistory = ({ children }) => {
     <Auth0Provider
       domain={domain}
       clientId={clientId}
-      redirectUri={window.location.origin}
+      redirectUri={`${window.location.origin}/dashboard`}
       onRedirectCallback={onRedirectCallback}
       cacheLocation="localstorage"
       audience={audience}
