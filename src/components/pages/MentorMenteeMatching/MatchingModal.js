a4372ced024b1cd191f9c4ff0ca30ce92c218ef6
Branden Jones <branden.wayne.jones@gmail.com>
2023-02-10 13:13:25 -0500

    mentee / mentor display in row for modal

--- src/components/pages/MentorMenteeMatching/MatchingModal.js
+++ src/components/pages/MentorMenteeMatching/MatchingModal.js
@@ -1,38 +1,105 @@
 import React from 'react';
-import { Modal, Row, Col } from 'antd';
-import MentorTable from './MentorTable';
-
-const MatchingModal = props => {
-  const { modal, handleCancel, handleSave, selectedMentors } = props;
-  const [selectedMentorKeys, setSelectedMentorKeys] = selectedMentors;
-
+import { Modal, Tag, Button } from 'antd';
+const MatchingModal = ({ matchShow, handleCancel, user }) => {
   return (
     <Modal
-      title={`Match Mentee ${modal.data?.name}`}
+      title={
+        <div className="header-api">
+          <p style={{ marginBottom: '0px' }}>Matching</p>
+          <p
+            className="cross"
+            style={{
+              marginBottom: '0px',
+              color: '#000',
+              cursor: 'pointer',
+              fontWeight: 'bolder',
+            }}
+            onClick={handleCancel}
+          >
+            X
+          </p>
+        </div>
+      }
       width={'80vw'}
-      visible={modal.show}
+      visible={matchShow}
       onCancel={handleCancel}
-      onOk={() => handleSave(modal.data.key, selectedMentorKeys)}
       okText={'Save Changes'}
       maskClosable={false}
+      footer={null}
+      className="UserModal"
     >
-      <Row>
-        <Col span={8}>
-          <p>{modal.data?.contact}</p>
-        </Col>
-        <Col span={8}>
-          <p>{modal.data?.location}</p>
-        </Col>
-        <Col span={8}>
-          <p>{modal.data?.stack}</p>
-        </Col>
-      </Row>
-      <MentorTable
-        selectedMentors={selectedMentors}
-        selectedMentee={modal.data}
-      />
+      <div className="MatchingModal">
+        <div className="UserTable MenteeTable">
+          <div>
+            <div span={24} className="customCol">
+              <div className="FieldTitle">Name</div>
+              <p className="FieldValue">{`${user?.first_name} ${user?.last_name}`}</p>
+              <Tag color={'blue'}>{}</Tag>
+            </div>
+            <div span={24} className="customCol">
+              <div className="FieldTitle">City/State/Country</div>
+              <p className="FieldValue">{`${user?.city} / ${user?.state} / ${user?.country}`}</p>
+            </div>
+            <div span={24} className="customCol">
+              <div className="FieldTitle">Mentorship Topics</div>
+              <p className="FieldValue">
+                {user?.tech_stack?.map(stack => (
+                  <>
+                    <span>{stack}</span> <br />
+                  </>
+                ))}
+              </p>
+            </div>
+            <div span={24} className="customCol">
+              <div className="FieldTitle">Other Topics</div>
+              <p className="FieldValue">{user?.other_info}</p>
+            </div>
+          </div>
+          <div>
+            <h4>Matches</h4>
+            <h4>Suggested Matches</h4>
+          </div>
+        </div>
+        <br />
+        <div className="UserTable">
+          <div span={24} className="customCol">
+            <div className="FieldTitle">Name</div>
+            <p className="FieldValue">{`${user?.first_name} ${user?.last_name}`}</p>
+            <Tag color={'blue'}>{}</Tag>
+          </div>
+          <div span={24} className="customCol">
+            <div className="FieldTitle">Email</div>
+            <p className="FieldValue">{`${user?.email}`}</p>
+          </div>
+          <div span={24} className="customCol">
+            <div className="FieldTitle">City/State/Country</div>
+            <p className="FieldValue">{`${user?.city} / ${user?.state} / ${user?.country}`}</p>
+          </div>
+          <div span={24} className="customCol">
+            <div className="FieldTitle">Current Company/Position</div>
+            <p className="FieldValue">{user?.current_company}</p>
+          </div>
+          <div span={24} className="customCol">
+            <div className="FieldTitle">Mentorship Topics</div>
+            <p className="FieldValue">
+              {user?.tech_stack?.map(stack => (
+                <>
+                  <span>{stack}</span> <br />
+                </>
+              ))}
+            </p>
+          </div>
+          <div span={24} className="customCol">
+            <div className="FieldTitle">Other Topics</div>
+            <p className="FieldValue">{user?.other_info}</p>
+          </div>
+          <div span={24} className="customCol">
+            <div className="FieldTitle">Anything Else?</div>
+            <p className="FieldValue">{user?.other_info}</p>
+          </div>
+        </div>
+      </div>
     </Modal>
   );
 };
-
 export default MatchingModal;
