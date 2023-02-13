5e5e1c9427911ca7bb71e2eb110307f41eed242e
Branden Jones <branden.wayne.jones@gmail.com>
2023-02-13 11:46:42 -0500

    added space between function and imports

--- src/components/pages/UserManagement/UserManagement.js
+++ src/components/pages/UserManagement/UserManagement.js
@@ -10,6 +10,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 import UserModal from './UserModal';
 import MatchingModal from '../MentorMenteeMatching/MatchingModal';
 import { useHistory } from 'react-router-dom';
+
 const UserManagement = () => {
   const [accounts, setAccounts] = useState([]);
   const [updatedProfile, setUpdatedProfile] = useState();
