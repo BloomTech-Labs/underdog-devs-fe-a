54299cd578adda59564c942a893653556d998624
Branden Jones <branden.wayne.jones@gmail.com>
2023-02-13 11:48:15 -0500

    added useState and slices to accomodate matchModal

--- src/components/pages/UserManagement/UserModal.jsx
+++ src/components/pages/UserManagement/UserModal.jsx
@@ -1,77 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Modal } from 'antd';
+import MatchingModal from '../MentorMenteeMatching/MatchingModal';
+
 const UserModal = ({ userShow, handleCancel, user }) => {
+  const [matchShow, setMatchShow] = useState(false);
   return (
-    <Modal
-      title={
-        <div className="header-api">
-          <p style={{ marginBottom: '0px' }}>User Detail</p>{' '}
-          <p
-            className="cross"
-            style={{
-              marginBottom: '0px',
-              color: '#000',
-              cursor: 'pointer',
-              fontWeight: 'bolder',
-            }}
-            onClick={handleCancel}
-          >
-            X
-          </p>
+    <>
+      <Modal
+        title={
+          <div className="header-api">
+            <p style={{ marginBottom: '0px' }}>User Detail</p>{' '}
+            <p
+              className="cross"
+              style={{
+                marginBottom: '0px',
+                color: '#000',
+                cursor: 'pointer',
+                fontWeight: 'bolder',
+              }}
+              onClick={handleCancel}
+            >
+              X
+            </p>
+          </div>
+        }
+        width={'80vw'}
+        visible={userShow}
+        onCancel={handleCancel}
+        okText={'Save Changes'}
+        maskClosable={false}
+        footer={null}
+        className="UserModal"
+      >
+        <div style={{ padding: '0px 20px' }}>
+          <span>{`Mentor / 3 Matches     `} </span>
+          <button onClick={() => <MatchingModal />}>Edit Matches</button>{' '}
         </div>
-      }
-      width={'80vw'}
-      visible={userShow}
-      onCancel={handleCancel}
-      okText={'Save Changes'}
-      maskClosable={false}
-      footer={null}
-      className="UserModal"
-    >
-      <div style={{ padding: '0px 20px' }}>
-        <span>Mentor / 3 Matches</span> <a>Edit Matches</a>{' '}
-      </div>
-      <div className="UserTable">
-        <div span={24} className="customCol">
-          <div className="FieldTitle">Name</div>
-          <p className="FieldValue">{`${user?.first_name} ${user?.last_name}`}</p>
+        <div className="UserTable">
+          <div span={24} className="customCol">
+            <div className="FieldTitle">Name</div>
+            <p className="FieldValue">{`${user?.first_name} ${user?.last_name}`}</p>
+          </div>
+          <div span={24} className="customCol">
+            <div className="FieldTitle">Email</div>
+            <p className="FieldValue">{`${user?.email}`}</p>
+          </div>
+          <div span={24} className="customCol">
+            <div className="FieldTitle">City/State/Country</div>
+            <p className="FieldValue">{`${user?.city} / ${user?.state} / ${user?.country}`}</p>
+          </div>
+          <div span={24} className="customCol">
+            <div className="FieldTitle">Current Company/Position</div>
+            <p className="FieldValue">{user?.current_company}</p>
+          </div>
+          <div span={24} className="customCol">
+            <div className="FieldTitle">Mentorship Topics</div>
+            <p className="FieldValue">
+              {user?.tech_stack?.map(stack => (
+                <>
+                  <span>{stack}</span> <br />
+                </>
+              ))}
+            </p>
+          </div>
+          <div span={24} className="customCol">
+            <div className="FieldTitle">Commit?</div>
+            <p className="FieldValue">{user?.commitment ? 'Yes' : 'No'}</p>
+          </div>
+          <div span={24} className="customCol">
+            <div className="FieldTitle">Hear about UD?</div>
+            <p className="FieldValue">{user?.referred_by}</p>
+          </div>
+          <div span={24} className="customCol">
+            <div className="FieldTitle">Anything else</div>
+            <p className="FieldValue">{user?.other_info}</p>
+          </div>
         </div>
-        <div span={24} className="customCol">
-          <div className="FieldTitle">Email</div>
-          <p className="FieldValue">{`${user?.email}`}</p>
-        </div>
-        <div span={24} className="customCol">
-          <div className="FieldTitle">City/State/Country</div>
-          <p className="FieldValue">{`${user?.city} / ${user?.state} / ${user?.country}`}</p>
-        </div>
-        <div span={24} className="customCol">
-          <div className="FieldTitle">Current Company/Position</div>
-          <p className="FieldValue">{user?.current_company}</p>
-        </div>
-        <div span={24} className="customCol">
-          <div className="FieldTitle">Mentorship Topics</div>
-          <p className="FieldValue">
-            {user?.tech_stack?.map(stack => (
-              <>
-                <span>{stack}</span> <br />
-              </>
-            ))}
-          </p>
-        </div>
-        <div span={24} className="customCol">
-          <div className="FieldTitle">Commit?</div>
-          <p className="FieldValue">{user?.commitment ? 'Yes' : 'No'}</p>
-        </div>
-        <div span={24} className="customCol">
-          <div className="FieldTitle">Hear about UD?</div>
-          <p className="FieldValue">{user?.referred_by}</p>
-        </div>
-        <div span={24} className="customCol">
-          <div className="FieldTitle">Anything else</div>
-          <p className="FieldValue">{user?.other_info}</p>
-        </div>
-      </div>
-    </Modal>
+      </Modal>
+
+      {/* <MatchingModal
+        matchShow={matchShow}
+        handleCancel={setMatchShow(false)}
+        user={user}
+      /> */}
+    </>
   );
 };
 export default UserModal;
