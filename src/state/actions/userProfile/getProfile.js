14db35c44722378ac1b5f6417d5114a4c47e14ff
Branden Jones <branden.wayne.jones@gmail.com>
2023-03-05 16:05:52 -0500

    fixed 404 issue

--- src/state/actions/userProfile/getProfile.js
+++ src/state/actions/userProfile/getProfile.js
@@ -2,8 +2,8 @@ import { setUserProfile } from './setUserProfile';
 import axios from 'axios';
 import { API_URL } from '../../../config';
 
-export const getProfile = role => async dispatch => {
-  const res = await axios.get(`${API_URL}profile/role/${role}`);
+export const getProfile = data => async dispatch => {
+  const res = await axios.get(`${API_URL}profile`);
   dispatch(setUserProfile(res.data));
   return res.data;
 };
