34f1321524b796f072271c2d37b3bcd0da7bcb41
Branden Jones <branden.wayne.jones@gmail.com>
2023-03-15 17:40:24 -0400

    wip still rendering diff sizes

--- src/components/pages/RoleApply/Apply.js
+++ src/components/pages/RoleApply/Apply.js
@@ -3,9 +3,25 @@ import './Styles/apply.css';
 import { Link } from 'react-router-dom';
 import { Card, Button, Row, Col, Space, Breadcrumb } from 'antd';
 import { LoginOutlined, IdcardOutlined } from '@ant-design/icons';
-
+import img1 from '../../../images/pexels-photo-7504837.png';
+import img2 from '../../../images/pexels-photo-1181337.png';
 import { useAuth0 } from '@auth0/auth0-react';
-
+const data = [
+  {
+    img: img1,
+    link: '/apply/mentee',
+    role: 'Mentee',
+    description:
+      'Interested in acquiring the skills to become a Software Engineer? Apply to our Mentee program today to start learning.',
+  },
+  {
+    img: img2,
+    link: '/apply/mentor',
+    role: 'Mentor',
+    description:
+      'Do you have a passion for teaching and Software Engineering? Apply to become a Mentor and help support our Mentees.',
+  },
+];
 const Apply = () => {
   const { Meta } = Card;
 
@@ -26,75 +42,45 @@ const Apply = () => {
       </Row>
 
       <Row className="row">
-        <Col className="col1">
-          <Card
-            className="card1"
-            hoverable
-            cover={
-              <img
-                alt="example"
-                className="card-img"
-                src="https://images.pexels.com/photos/7504837/pexels-photo-7504837.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
-                // style={{ height: '100%' }}
-              />
-            }
-          >
-            <Space
-              className="outer-space-container"
-              direction="vertical"
-              align="baseline"
-            >
-              <Space direction="vertical">
-                <Meta className="title" description="Join our MENTEE program" />
-                <Meta
-                  className="description"
-                  description="Interested in acquiring the skills to become a Software Engineer? Apply to our Mentee program today to start learning."
-                />
-              </Space>
+        {data.map(col => {
+          return (
+            <Col className="col1">
+              <Card
+                className="card1"
+                hoverable
+                cover={<img alt="example" className="card-img" src={col.img} />}
+              >
+                <Space
+                  className="outer-space-container"
+                  direction="vertical"
+                  align="baseline"
+                >
+                  <Space direction="vertical">
+                    <Meta
+                      className="title"
+                      description={`Join our ${col.role.toUpperCase()} program`}
+                    />
+                    <Meta
+                      className="description"
+                      description={col.description}
+                    />
+                  </Space>
 
-              <Link to="/apply/mentee">
-                <Button className="button" type="primary" size="large" block>
-                  Apply as a Mentee
-                </Button>
-              </Link>
-            </Space>
-          </Card>
-        </Col>
-
-        <Col className="col2">
-          <Card
-            hoverable
-            className="card2"
-            cover={
-              <img
-                alt="example"
-                className="card-img"
-                src="https://images.pexels.com/photos/1181337/pexels-photo-1181337.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
-                // style={{ height: '100%' }}
-              />
-            }
-          >
-            <Space
-              className="outer-space-container"
-              direction="vertical"
-              align="baseline"
-            >
-              <Space direction="vertical">
-                <Meta className="title" description="Join our MENTOR program" />
-                <Meta
-                  className="description"
-                  description="Do you have a passion for teaching and Software Engineering? Apply to become a Mentor and help support our Mentees."
-                />
-              </Space>
-
-              <Link to="/apply/mentor">
-                <Button className="button" type="primary" size="large" block>
-                  Apply as a Mentor
-                </Button>
-              </Link>
-            </Space>
-          </Card>
-        </Col>
+                  <Link to={col.link}>
+                    <Button
+                      className="button"
+                      type="primary"
+                      size="large"
+                      block
+                    >
+                      Apply as a {col.role}
+                    </Button>
+                  </Link>
+                </Space>
+              </Card>
+            </Col>
+          );
+        })}
       </Row>
     </div>
   );
