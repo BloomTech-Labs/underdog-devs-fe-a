3681cee57343b7b1ac014542a1ce511b0b15ede7
Branden Jones <branden.wayne.jones@gmail.com>
2023-03-23 13:33:01 -0400

    cleanup

--- src/state/actions/allUsers/getAllUsers.js
+++ src/state/actions/allUsers/getAllUsers.js
@@ -3,9 +3,7 @@ import axios from 'axios';
 import { API_URL } from '../../../config';
 
 export const getAllUsers = () => async dispatch => {
-  console.log(`Hello from getALLUSERS`);
   const res = await axios.get(`${API_URL}users/get/all`);
-  console.log(`WHAT IS THIS?`, res);
   dispatch(setAllUsers(res.data));
   return res.data;
 };
