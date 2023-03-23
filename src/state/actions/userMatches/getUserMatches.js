f5e96391113e44fbc373f28423c803d1290c7915
Branden Jones <branden.wayne.jones@gmail.com>
2023-03-23 17:48:36 -0400

    API call wip

---
+++ src/state/actions/userMatches/getUserMatches.js
@@ -0,0 +1,23 @@
+import { setUserMatches } from './setUserMatches';
+import axios from 'axios';
+import { API_URL } from '../../../config';
+
+export const getUserMatches = (arrOfProfileIDs, role) => async dispatch => {
+  role === 'mentor' ? (role = 'mentee') : (role = 'mentor');
+  const results = [];
+  let n = 0;
+  while (n < arrOfProfileIDs.length) {
+    const res = await axios({
+      method: 'post',
+      url: `${API_URL}users/read/${role}`,
+      data: {
+        profile_id: arrOfProfileIDs[n],
+      },
+    });
+    console.log(res.data, n);
+    results.push(res.data);
+    n++;
+  }
+  dispatch(setUserMatches(results));
+  return results;
+};
