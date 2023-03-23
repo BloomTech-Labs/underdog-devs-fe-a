883c5caf6b5cd20a49139e245a9766e43265cbf7
Branden Jones <branden.wayne.jones@gmail.com>
2023-03-23 15:57:59 -0400

    creator grabbing specific role

--- src/state/actions/allUsers/getAllUsers.js
+++ src/state/actions/allUsers/getAllUsers.js
@@ -2,8 +2,8 @@ import { setAllUsers } from './setAllUsers';
 import axios from 'axios';
 import { API_URL } from '../../../config';
 
-export const getAllUsers = () => async dispatch => {
-  const res = await axios.get(`${API_URL}users/get/all`);
+export const getAllUsers = role => async dispatch => {
+  const res = await axios.post(`${API_URL}users/read/${role}`);
   dispatch(setAllUsers(res.data));
   return res.data;
 };
