1678dcf51e0e963603dcd56bda9ca7f54da2d01d
Branden Jones <branden.wayne.jones@gmail.com>
2023-03-24 16:52:33 -0400

    matches section working

--- src/components/pages/MentorMenteeMatching/MatchingModal.js
+++ src/components/pages/MentorMenteeMatching/MatchingModal.js
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Modal, Tag, Button, Divider } from 'antd';
 import { useDispatch, connect } from 'react-redux';
 import { getUserMatches } from '../../../state/actions/userMatches/getUserMatches';
 
 const MatchingModal = ({ matchShow, handleCancel, user, allUserMatches }) => {
+  const [currentMatch, setCurrentMatch] = useState(allUserMatches[0]);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -71,22 +72,28 @@ const MatchingModal = ({ matchShow, handleCancel, user, allUserMatches }) => {
           <div className="MatchSuggestMatch">
             <div className="Matches">
               <h4>Matches</h4>
-              <Divider style={{ margin: '8px 0' }} />
-              <div className="matchLine">
-                <p>Match One</p>
-                <p style={{ color: 'blue' }}>View</p>
-              </div>
-              <Divider style={{ margin: '8px 0' }} />
-              <div className="matchLine">
-                <p>Match Two</p>
-                <p style={{ color: 'blue' }}>View</p>
-              </div>
-              <Divider style={{ margin: '8px 0' }} />
-              <div className="matchLine">
-                <p>Match Three</p>
-                <p style={{ color: 'blue' }}>View</p>
-              </div>
-              <Divider style={{ margin: '8px 0' }} />
+              {allUserMatches
+                ? allUserMatches.map(row => {
+                    return (
+                      <>
+                        <Divider style={{ margin: '8px 0' }} />
+                        <div className="matchLine">
+                          <p>{`${row.first_name} ${row.last_name}`}</p>
+                          <p
+                            style={{ color: 'blue' }}
+                            onClick={() =>
+                              allUserMatches
+                                ? setCurrentMatch(allUserMatches[row])
+                                : null
+                            }
+                          >
+                            View
+                          </p>
+                        </div>
+                      </>
+                    );
+                  })
+                : null}
             </div>
 
             <div className="Suggestions">
@@ -129,7 +136,7 @@ const MatchingModal = ({ matchShow, handleCancel, user, allUserMatches }) => {
           <div span={24} className="customCol">
             <div className="FieldTitle">Name</div>
             <p className="FieldValue">
-              {`${user?.first_name} ${user?.last_name}`}
+              {`${currentMatch.first_name} ${currentMatch.last_name}`}
               <div className="userTag">
                 <Tag color="blue">
                   {user?.role === 'mentee' || user?.role === 'Mentee'
@@ -142,27 +149,29 @@ const MatchingModal = ({ matchShow, handleCancel, user, allUserMatches }) => {
 
           <div span={24} className="customCol">
             <div className="FieldTitle">Email</div>
-            <p className="FieldValue">{`${user?.email}`}</p>
+            <p className="FieldValue">{`${currentMatch.email}`}</p>
           </div>
           <div span={24} className="customCol">
             <div className="FieldTitle">City/ State/ Country</div>
-            <p className="FieldValue">{`${user?.city} / ${user?.state} / ${user?.country}`}</p>
+            <p className="FieldValue">{`${currentMatch.city} / ${currentMatch.state} / ${currentMatch.country}`}</p>
           </div>
           <div span={24} className="customCol">
             <div className="FieldTitle">Current Company/ Position</div>
-            <p className="FieldValue">{user?.current_company}</p>
+            <p className="FieldValue">{currentMatch.current_company}</p>
           </div>
           <div span={24} className="customCol">
             <div className="FieldTitle">Mentorship Topics</div>
-            <p className="FieldValue">{<span>{user?.tech_stack}</span>}</p>
+            <p className="FieldValue">
+              {<span>{currentMatch.tech_stack}</span>}
+            </p>
           </div>
           <div span={24} className="customCol">
             <div className="FieldTitle">Other Topics</div>
-            <p className="FieldValue">{user?.other_info}</p>
+            <p className="FieldValue">{currentMatch.other_info}</p>
           </div>
           <div span={24} className="customCol">
             <div className="FieldTitle">Anything Else?</div>
-            <p className="FieldValue">{user?.other_info}</p>
+            <p className="FieldValue">{currentMatch.other_info}</p>
           </div>
         </div>
       </div>
