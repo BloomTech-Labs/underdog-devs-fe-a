50e9f1ce10b9e97b0946ddb8f83832f8bdb6d0c7
Branden Jones <branden.wayne.jones@gmail.com>
2023-04-04 16:04:17 -0400

    dashboard displays matches and userDetails

--- src/components/pages/MyMentees/MyMentees.js
+++ src/components/pages/MyMentees/MyMentees.js
@@ -1,40 +1,43 @@
 import React, { useEffect, useState } from 'react';
-import useAxiosWithAuth0 from '../../../hooks/useAxiosWithAuth0';
 import { List } from 'antd';
-import { connect } from 'react-redux';
-import dummyData from './data.json';
+import { useDispatch, connect } from 'react-redux';
+import { getUserMatches } from '../../../state/actions/userMatches/getUserMatches';
+import UserModal from '../UserManagement/UserModal';
 
-const MyMentees = props => {
-  const { axiosWithAuth } = useAxiosWithAuth0();
-  const { role, profile_id } = props;
-  // eslint-disable-next-line no-unused-vars
-  const [data, setData] = useState([]);
+const MyMentees = ({ currentUser, userMatches }) => {
+  const [userShow, setUserShow] = useState(false);
+  const [user, setUser] = useState('');
+  const dispatch = useDispatch();
 
+  console.log(currentUser);
   useEffect(() => {
-    axiosWithAuth()
-      .get(`/assignments/${role}/${profile_id}`)
-      .then(res => {
-        setData(res.data);
-      })
-      .catch(err => {
-        console.error(err);
-      });
-  });
+    dispatch(getUserMatches(currentUser.matches, currentUser.role));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <>
       <div>
-        <h2>My Mentees</h2>
+        <h2>My {currentUser.role === 'mentor' ? 'Mentees' : 'Mentors'}</h2>
         <List
           itemLayout="horizontal"
-          dataSource={dummyData}
+          dataSource={userMatches}
           renderItem={item => (
             <List.Item>
               <List.Item.Meta
                 title={`${item.first_name} ${item.last_name}`}
                 description={item.email}
+                onClick={() => {
+                  setUser(item);
+                  setUserShow(true);
+                }}
               />
             </List.Item>
           )}
+        />
+        <UserModal
+          userShow={userShow}
+          handleCancel={() => setUserShow(false)}
+          user={user}
         />
       </div>
     </>
@@ -47,8 +50,8 @@ Ticket BL-1042 addresses this requirement
 */
 const mapStateToProps = state => {
   return {
-    profile_id: 'ea9ff763-71b7-4235-bc18-2b148a272a1e',
-    role: 'mentor',
+    currentUser: state.user.currentUser,
+    userMatches: state.user.allUserMatches,
   };
 };
 
