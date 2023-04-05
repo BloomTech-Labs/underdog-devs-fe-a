870351c3052afc45d1f814eb87551e133a73358c
Branden Jones <branden.wayne.jones@gmail.com>
2023-04-05 10:06:20 -0400

    rendering errors fixed

--- src/state/actions/userMatches/getUserMatches.js
+++ src/state/actions/userMatches/getUserMatches.js
@@ -3,21 +3,25 @@ import axios from 'axios';
 import { API_URL } from '../../../config';
 
 export const getUserMatches = (arrOfProfileIDs, role) => async dispatch => {
-  role === 'mentor' ? (role = 'mentee') : (role = 'mentor');
-  const results = [];
-  let n = 0;
-  if (arrOfProfileIDs.length > 0) {
-    while (n < arrOfProfileIDs.length) {
-      const res = await axios({
-        method: 'post',
-        url: `${API_URL}matches/read/${role}`,
-        data: {
-          profile_id: arrOfProfileIDs[n],
-        },
-      });
-      results.push(res.data[0]);
-      n++;
+  if (arrOfProfileIDs) {
+    role === 'mentor' ? (role = 'mentee') : (role = 'mentor');
+    const results = [];
+    let n = 0;
+    if (arrOfProfileIDs.length > 0) {
+      while (n < arrOfProfileIDs.length) {
+        const res = await axios({
+          method: 'post',
+          url: `${API_URL}matches/read/${role}`,
+          data: {
+            profile_id: arrOfProfileIDs[n],
+          },
+        });
+        results.push(res.data[0]);
+        n++;
+      }
     }
+    return dispatch(setUserMatches(results));
+  } else {
+    return;
   }
-  return dispatch(setUserMatches(results));
 };
