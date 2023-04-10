a6b6725c2d06d55f703d5709ae822d98f1749eb6
Branden Jones <branden.wayne.jones@gmail.com>
2023-04-10 10:51:38 -0400

    fixing display for no matches

--- src/state/actions/allUsers/setAllUsers.js
+++ src/state/actions/allUsers/setAllUsers.js
@@ -1,3 +1,5 @@
+import { Tag } from 'antd';
+
 export const SET_ALL_MENTORS = 'SET_ALL_MENTORS';
 export const SET_ALL_MENTEES = 'SET_ALL_MENTEES';
 
@@ -5,7 +7,12 @@ export const setAllUsers = (list, role) => {
   let payload = list.map(row => {
     return {
       name: `${row.first_name} ${row.last_name}`,
-      numberOfMatches: row.matches ? row.matches.length : 0,
+      numberOfMatches:
+        row.matches && row.matches.length > 0 ? (
+          row.matches.length
+        ) : (
+          <Tag color={'orange'}>No Matches</Tag>
+        ),
       role: role === 'mentor' ? 'Mentor' : 'Mentee',
       ...row,
     };
