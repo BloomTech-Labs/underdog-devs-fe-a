b4f4fb35a50eb847e97d088989758a8ada82e13f
jesswillcode <jesswillcode@gmail.com>
2023-05-22 20:27:26 -0400

    mentee tests almost complete

--- src/__tests__/MentorDashboard.test.js
+++ src/__tests__/MentorDashboard.test.js
@@ -1,3 +1,11 @@
 test.skip('Home Container no longer exists, this entire test needs to be revisited/refactored or deleted. It was originally for the Mentor Dashboard Test', () => {
   console.log('Revisit this test');
 });
+
+//component loads
+
+//shows appropriate welcome message
+
+//shows matched mentees
+
+//clicking on a mentee opens modal with mentee information
