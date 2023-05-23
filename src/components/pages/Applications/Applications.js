2ea79f8be22ae35deff059a5f571054c86835b9b
paulstgermain <paulg.stgermain@gmail.com>
2023-05-23 14:49:57 -0400

    updates applications page to use "created_at", not "updated_at"

--- src/components/pages/Applications/Applications.js
+++ src/components/pages/Applications/Applications.js
@@ -111,7 +111,7 @@ const Applications = () => {
               other_info: row.other_info,
               full_name: row.first_name + ' ' + row.last_name,
               role: <p>{uppercaseRole(row.role_name)}</p>,
-              date: convertDate(row.updated_at),
+              date: convertDate(row.created_at),
               status: (
                 <Tag
                   color={
@@ -171,7 +171,7 @@ const Applications = () => {
               other_info: row.other_info,
               full_name: row.first_name + ' ' + row.last_name,
               role: <p>{uppercaseRole(row.role_name)}</p>,
-              date: convertDate(row.updated_at),
+              date: convertDate(row.created_at),
               status: (
                 <Tag
                   color={
